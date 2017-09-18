106f1055575bbc9a2d27fa3b41a42b8801a372f0
Tomer <tomer@corevo.io>
2017-09-18 12:09:05 +0300

    fixed indentation

--- src/prompt-injector.js
+++ src/prompt-injector.js
@@ -21,52 +21,52 @@ elementForInjectingScript.src = browser.runtime.getURL("/assets/prompt.js");
 
 
 if (window === window.top) {
-    window.addEventListener("message", function(event) {
-        if (event.source.top == window && event.data &&
-            event.data.direction == "from-page-script") {
-            if (event.data.recordedType) {
-                switch (event.data.recordedType) {
-                    case "prompt":
-                        if (event.data.recordedResult != null) {
-                            record("answerOnNextPrompt", [[event.data.recordedResult]], "", true, event.data.frameLocation);
-                        } else {
-                            record("chooseCancelOnNextPrompt", [[""]], "", true, event.data.frameLocation);
-                        }
-                        record("assertPrompt", [[event.data.recordedMessage]], "", false, event.data.frameLocation);
-                        break;
-                    case "confirm":
-                        if (event.data.recordedResult == true) {
-                            record("chooseOkOnNextConfirmation", [[""]], "", true, event.data.frameLocation);
-                        } else {
-                            record("chooseCancelOnNextConfirmation", [[""]], "", true, event.data.frameLocation);
-                        }
-                        record("assertConfirmation", [[event.data.recordedMessage]], "", false, event.data.frameLocation);
-                        break;
-                    case "alert":
-                        //record("answerOnNextAlert",[[event.data.recordedResult]],"",true);
-                        record("assertAlert", [[event.data.recordedMessage]], "", false, event.data.frameLocation);
-                        break;
-                }
+  window.addEventListener("message", function(event) {
+    if (event.source.top == window && event.data &&
+      event.data.direction == "from-page-script") {
+      if (event.data.recordedType) {
+        switch (event.data.recordedType) {
+          case "prompt":
+            if (event.data.recordedResult != null) {
+              record("answerOnNextPrompt", [[event.data.recordedResult]], "", true, event.data.frameLocation);
+            } else {
+              record("chooseCancelOnNextPrompt", [[""]], "", true, event.data.frameLocation);
             }
-            if (event.data.response) {
-                switch (event.data.response) {
-                    case "prompt":
-                        selenium.browserbot.promptResponse = true;
-                        if (event.data.value)
-                            selenium.browserbot.promptMessage = event.data.value;
-                        break;
-                    case "confirm":
-                        selenium.browserbot.confirmationResponse = true;
-                        if (event.data.value)
-                            selenium.browserbot.confirmationMessage = event.data.value;
-                        break;
-                    case "alert":
-                        selenium.browserbot.alertResponse = true;
-                        if(event.data.value)
-                            selenium.browserbot.alertMessage = event.data.value;
-                        break;
-                }
+            record("assertPrompt", [[event.data.recordedMessage]], "", false, event.data.frameLocation);
+            break;
+          case "confirm":
+            if (event.data.recordedResult == true) {
+              record("chooseOkOnNextConfirmation", [[""]], "", true, event.data.frameLocation);
+            } else {
+              record("chooseCancelOnNextConfirmation", [[""]], "", true, event.data.frameLocation);
             }
+            record("assertConfirmation", [[event.data.recordedMessage]], "", false, event.data.frameLocation);
+            break;
+          case "alert":
+            //record("answerOnNextAlert",[[event.data.recordedResult]],"",true);
+            record("assertAlert", [[event.data.recordedMessage]], "", false, event.data.frameLocation);
+            break;
         }
-    })
+      }
+      if (event.data.response) {
+        switch (event.data.response) {
+          case "prompt":
+            selenium.browserbot.promptResponse = true;
+            if (event.data.value)
+              selenium.browserbot.promptMessage = event.data.value;
+            break;
+          case "confirm":
+            selenium.browserbot.confirmationResponse = true;
+            if (event.data.value)
+              selenium.browserbot.confirmationMessage = event.data.value;
+            break;
+          case "alert":
+            selenium.browserbot.alertResponse = true;
+            if(event.data.value)
+              selenium.browserbot.alertMessage = event.data.value;
+            break;
+        }
+      }
+    }
+  });
 }
