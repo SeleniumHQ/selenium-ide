cd3dff85f8cbb80a29ac249d9eb9ed1bb7e59db9
Tomer <tomer@corevo.io>
2017-09-24 16:15:12 +0300

    react default eslint preset

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     "node": true,
     "es6": true
   },
-  "extends": "eslint:recommended",
+  "extends": ["eslint:recommended", "plugin:react/recommended"],
   "parserOptions": {
     "ecmaFeatures": {
       "experimentalObjectRestSpread": true,
