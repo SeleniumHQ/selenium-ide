c1af6a2e5c4b0aca4a1e29175fdfe7154bd179c1
Tomer <tomer@corevo.io>
2017-09-25 14:26:02 +0300

    future styling

--- src/neo/components/ProjectList/index.jsx
+++ src/neo/components/ProjectList/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Project from "../Project";
+import "./style.css";
 
 export default class ProjectList extends React.Component {
   static propTypes = {
@@ -8,10 +9,10 @@ export default class ProjectList extends React.Component {
   };
   render() {
     return (
-      <ul>
+      <ul className="projects">
         {this.props.projects.map(project => (
-          <li key = {project}>
-            <Project name = {project} />
+          <li key={project}>
+            <Project name={project} />
           </li>
         ))}
       </ul>
