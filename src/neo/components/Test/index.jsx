758b2e79fa3e1ddad7a050fa27c813a2b3c3c606
Tomer <tomer@corevo.io>
2017-09-25 17:01:32 +0300

    selection background

--- src/neo/components/Test/index.jsx
+++ src/neo/components/Test/index.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 import test from "../../images/ic_test.svg";
 import "./style.css";
 
 export default class Test extends React.Component {
   static propTypes = {
-    name: PropTypes.string.isRequired
+    name: PropTypes.string.isRequired,
+    selected: PropTypes.bool
   };
   render() {
     return (
-      <a href="#" className="test">
+      <a href="#" className={classNames("test", {"selected": this.props.selected})}>
         <img src={test} alt="test" />
         <span>{this.props.name}</span>
       </a>
