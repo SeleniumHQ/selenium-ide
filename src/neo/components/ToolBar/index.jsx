97a0884dcbeecd693bc00b3dbbce664935e953b8
Tomer <tomer@corevo.io>
2017-10-01 17:25:10 +0300

    added play current button

--- src/neo/components/ToolBar/index.jsx
+++ src/neo/components/ToolBar/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import PlayAll from "../../components/ActionButtons/PlayAll";
+import PlayCurrent from "../../components/ActionButtons/PlayCurrent";
 
 export default class ToolBar extends React.Component {
   render() {
     return (
       <span>
         <PlayAll />
+        <PlayCurrent />
       </span>
     );
   }
