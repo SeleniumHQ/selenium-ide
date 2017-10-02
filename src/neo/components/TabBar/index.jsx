45ae2b3b4fd16d12ad1abebc18f731ee1e1793e5
Tomer <tomer@corevo.io>
2017-10-02 16:33:08 +0300

    default tab and selected tab logic

--- src/neo/components/TabBar/index.jsx
+++ src/neo/components/TabBar/index.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 import "./style.css";
 
 export default class TabBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: props.defaultTab ? props.defaultTab : props.tabs[0]
+    };
+  }
   static propTypes = {
-    tabs: PropTypes.array.isRequired
+    tabs: PropTypes.array.isRequired,
+    defaultTab: PropTypes.string
   };
+  handleClick(tab) {
+    this.setState({
+      activeTab: tab
+    });
+  }
   render() {
     return (
       <ul className="tabbar">
         {this.props.tabs.map((tab) => (
           <li key={tab}>
-            <a href="#">{tab}</a>
+            <a href="#" className={classNames({"active": this.state.activeTab === tab})} onClick={this.handleClick.bind(this, tab)}>{tab}</a>
           </li>
         ))}
       </ul>
