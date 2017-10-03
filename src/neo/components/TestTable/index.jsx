076e1056025b25ee8c30d14aebb67ca28918cacd
Tomer <tomer@corevo.io>
2017-10-03 15:25:53 +0300

    table body and border bottom

--- src/neo/components/TestTable/index.jsx
+++ src/neo/components/TestTable/index.jsx
@@ -12,6 +12,8 @@ export default class TestTable extends React.Component {
             <th>Value</th>
           </tr>
         </thead>
+        <tbody>
+        </tbody>
       </table>
     );
   }
