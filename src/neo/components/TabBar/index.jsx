eafd3034aa696b469eb8552ab2724777b5dde1cb
Tomer <tomer@corevo.io>
2017-10-03 11:18:14 +0300

    tabbar underline animation

--- src/neo/components/TabBar/index.jsx
+++ src/neo/components/TabBar/index.jsx
@@ -1,33 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
-import classNames from "classnames";
 import "./style.css";
 
 export default class TabBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeTab: props.defaultTab ? props.defaultTab : props.tabs[0]
+      activeTab: props.defaultTab ? {
+        tab: props.defaultTab,
+        index: props.tabs.indexOf(props.defaultTab)
+      } : {
+        tab: props.tabs[0],
+        index: 0
+      }
     };
   }
   static propTypes = {
     tabs: PropTypes.array.isRequired,
     defaultTab: PropTypes.string
   };
-  handleClick(tab) {
+  handleClick(tab, index) {
     this.setState({
-      activeTab: tab
+      activeTab: { tab, index }
     });
   }
   render() {
     return (
-      <ul className="tabbar">
-        {this.props.tabs.map((tab) => (
-          <li key={tab}>
-            <a href="#" className={classNames({"active": this.state.activeTab === tab})} onClick={this.handleClick.bind(this, tab)}>{tab}</a>
-          </li>
-        ))}
-      </ul>
+      <div className="tabbar">
+        <ul>
+          {this.props.tabs.map((tab, index) => (
+            <li key={tab}>
+              <a href="#" onClick={this.handleClick.bind(this, tab, index)}>{tab}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="underline" style={{
+          transform: `translateX(${this.state.activeTab.index * 80}px)`
+        }}></div>
+      </div>
     );
   }
 }
