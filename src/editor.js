477393c016fbc6488c1a6790481ae6be26ca6d05
Doron Zavelevsky <zavelevsky@gmail.com>
2017-10-04 15:29:21 +0300

    proper notification logo

--- src/editor.js
+++ src/editor.js
@@ -329,7 +329,7 @@ function notification(command, target, value) {
     // In Chrome, notification.create must have "iconUrl" key in notificationOptions
     browser.notifications.create(tempCount, {
         "type": "basic",
-        "iconUrl": "/icons/icons-48.png",
+        "iconUrl": "/icons/icon.png",
         "title": "Record command!",
         "message": "command: " + String(command) + "\ntarget: " + String(target[0][0]) + "\nvalue: " + String(value) 
     });
