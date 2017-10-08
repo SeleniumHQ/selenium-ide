a3698a467c919ff4bf72c7464aa7b89be5f215bc
Tomer <tomer@corevo.io>
2017-10-08 11:02:21 +0300

    added test state

--- src/neo/components/TestRow/index.jsx
+++ src/neo/components/TestRow/index.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
 import "./style.css";
 
+export const RowState = {
+  Passed: "passed",
+  Failed: "failed"
+};
+
 export default class TestRow extends React.Component {
+  static propTypes = {
+    state: PropTypes.oneOf(Object.keys(RowState))
+  };
   render() {
     return (
-      <tr>
+      <tr className={classNames({[this.props.state]: this.props.state})}>
         <td>open</td>
         <td>/</td>
         <td></td>
