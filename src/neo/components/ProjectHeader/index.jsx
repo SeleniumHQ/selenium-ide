d705580ade652415aca8c0ea0dfafbd559fb6f9e
Tomer <tomer@corevo.io>
2017-10-10 13:23:34 +0300

    set the default title

--- src/neo/components/ProjectHeader/index.jsx
+++ src/neo/components/ProjectHeader/index.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import Title from "react-document-title";
 import "./style.css";
 
 export default class ProjectHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Untitled Project"
+    };
+  }
   render() {
     return (
       <div className="header">
-        <input type="text" defaultValue="Untitled Project" placeholder="Project Name" />
+        <Title title={`Selenium IDE - ${this.state.title}`} />
+        <input type="text" defaultValue={this.state.title} placeholder="Project Name" />
       </div>
     );
   }
