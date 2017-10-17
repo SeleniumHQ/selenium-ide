6922a6366ae293115b4203e3113ab9d4c5ba1ba1
Tomer <tomer@corevo.io>
2017-10-17 10:44:55 +0300

    add observe test

--- src/neo/__test__/stores/domain/ProjectStore.spec.js
+++ src/neo/__test__/stores/domain/ProjectStore.spec.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+import { autorun } from "mobx";
 import ProjectStore from "../../../stores/domain/ProjectStore";
 import Test from "../../../models/Test";
 
@@ -6,6 +7,14 @@ describe("Project Store", () => {
   it("should have a name", () => {
     const store = new ProjectStore("myStore");
     expect(store.name).toBe("myStore");
+  });
+  it("should observe the name change", () => {
+    const store = new ProjectStore("myStore");
+    store.name = "changed";
+    const disposer = autorun(function() {
+      expect(store.name).toBe("changed");
+    });
+    disposer();
   });
   it("should add test to the store", () => {
     const store = new ProjectStore();
