61e74bf3b9cf0fa823eb27cd2ffb5bd7bdf74bca
Tomer <tomer@corevo.io>
2017-10-17 12:30:57 +0300

    addTest function

--- src/neo/stores/domain/ProjectStore.js
+++ src/neo/stores/domain/ProjectStore.js
@@ -7,5 +7,14 @@ export default class ProjectStore {
 
   constructor(name) {
     this.name = name;
+    this.addTest = this.addTest.bind(this);
+  }
+
+  addTest(test) {
+    if (!test || test.constructor.name !== "Test") {
+      throw new Error(`Expected to receive Test instead received ${test ? test.constructor.name : test}`);
+    } else {
+      this.tests.push(test);
+    }
   }
 }
