dfce38f6a5a818766554e903809d36e8ad6e9c72
Tomer <tomer@corevo.io>
2017-10-17 10:24:41 +0300

    added name test

--- src/neo/__test__/stores/domain/ProjectStore.spec.js
+++ src/neo/__test__/stores/domain/ProjectStore.spec.js
@@ -3,6 +3,10 @@ import ProjectStore from "../../../stores/domain/ProjectStore";
 import Test from "../../../models/Test";
 
 describe("Project Store", () => {
+  it("should have a name", () => {
+    const store = new ProjectStore("myStore");
+    expect(store.name).toBe("myStore");
+  });
   it("should add test to the store", () => {
     const store = new ProjectStore();
     store.tests.push(new Test());
