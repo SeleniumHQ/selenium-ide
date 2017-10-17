08fd28f9cc4f360575aa76cbdfeceefe00e76ce0
Tomer <tomer@corevo.io>
2017-10-17 15:39:44 +0300

    Delete test

--- src/neo/stores/domain/ProjectStore.js
+++ src/neo/stores/domain/ProjectStore.js
@@ -3,7 +3,7 @@ import { observable } from "mobx";
 export default class ProjectStore {
   @observable name = "";
   @observable tests = [];
-  @observable suite = [];
+  @observable suites = [];
 
   constructor(name) {
     this.name = name;
@@ -15,6 +15,17 @@ export default class ProjectStore {
       throw new Error(`Expected to receive Test instead received ${test ? test.constructor.name : test}`);
     } else {
       this.tests.push(test);
+    }
+  }
+
+  deleteTest(test) {
+    if (!test || test.constructor.name !== "Test") {
+      throw new Error(`Expected to receive Test instead received ${test ? test.constructor.name : test}`);
+    } else {
+      this.suites.forEach(suite => {
+        suite.removeTest(test);
+      });
+      this.tests.remove(test);
     }
   }
 }
