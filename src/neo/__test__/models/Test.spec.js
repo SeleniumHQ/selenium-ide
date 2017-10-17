051c677e09f09794badebbf31d694c7ae284b29c
Tomer <tomer@corevo.io>
2017-10-17 10:53:16 +0300

    Test id test

--- src/neo/__test__/models/Test.spec.js
+++ src/neo/__test__/models/Test.spec.js
@@ -5,4 +5,7 @@ describe("Test model", () => {
   it("new test should be named 'Utitled Test'", () => {
     expect((new Test()).name).toBe("Untitled Test");
   });
+  it("Tests should have randomly generated identifiers", () => {
+    expect((new Test()).id).not.toBe((new Test()).id);
+  });
 });
