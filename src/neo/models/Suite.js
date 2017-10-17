762d24c7aeb6babf142f7990c6cc1ff0b1569980
Tomer <tomer@corevo.io>
2017-10-17 11:48:12 +0300

    addTest function

--- src/neo/models/Suite.js
+++ src/neo/models/Suite.js
@@ -11,5 +11,14 @@ export default class Suite {
   constructor(store, id = uuidv4()) {
     this.store = store;
     this.id = id;
+    this.addTest = this.addTest.bind(this);
+  }
+
+  addTest(test) {
+    if (!test || test.constructor.name !== "Test") {
+      throw new Error(`Expected to receive Test instead received ${test ? test.constructor.name : test}`);
+    } else {
+      this.tests.push(test);
+    }
   }
 }
