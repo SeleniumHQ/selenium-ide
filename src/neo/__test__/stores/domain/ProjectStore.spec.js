9ea49101850c343a0721d29823d84b069f55f7f6
Tomer <tomer@corevo.io>
2017-10-17 15:19:02 +0300

    added delete test tests

--- src/neo/__test__/stores/domain/ProjectStore.spec.js
+++ src/neo/__test__/stores/domain/ProjectStore.spec.js
@@ -38,4 +38,33 @@ describe("Project Store", () => {
     const store = new ProjectStore();
     expect(() => store.addTest(1)).toThrowError("Expected to receive Test instead received Number");
   });
+  it("should delete a test", () => {
+    const store = new ProjectStore();
+    const test = new Test();
+    store.addTest(test);
+    expect(store.tests.length).toBe(1);
+    store.deleteTest(test);
+    expect(store.tests.length).toBe(0);
+  });
+  it("should remove the deleted test from it's suites", () => {
+    const store = new ProjectStore();
+    const firstSuite = new Suite(store);
+    const secondSuite = new Suite(store);
+    const controlSuite = new Suite(store);
+    const toBeDeleted = new Test();
+    const control = new Test();
+    store.addTest(toBeDeleted);
+    store.addTest(control);
+    firstSuite.addTest(toBeDeleted);
+    secondSuite.addTest(toBeDeleted);
+    secondSuite.addTest(control);
+    controlSuite.addTest(control);
+    expect(firstSuite.tests.length).toBe(1);
+    expect(secondSuite.tests.length).toBe(2);
+    expect(controlSuite.tests.length).toBe(1);
+    store.deleteTest(toBeDeleted);
+    expect(firstSuite.tests.length).toBe(0);
+    expect(secondSuite.tests.length).toBe(1);
+    expect(controlSuite.tests.length).toBe(1);
+  });
 });
