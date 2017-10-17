7dc5926e27c11c2f731fc7ae1d3ee8e8e3d9eaa9
Tomer <tomer@corevo.io>
2017-10-17 11:11:11 +0300

    added name test

--- src/neo/__test__/models/Test.spec.js
+++ src/neo/__test__/models/Test.spec.js
@@ -1,9 +1,18 @@
 /* eslint-disable */
+import { observe } from "mobx";
 import Test from "../../models/Test";
 
 describe("Test model", () => {
   it("new test should be named 'Utitled Test'", () => {
     expect((new Test()).name).toBe("Untitled Test");
+  });
+  it("should observe name changes", () => {
+    const test = new Test();
+    const disposer = observe(test, "name", (change) => {
+      expect(change.newValue).toBe("changed");
+    });
+    test.name = "changed";
+    disposer();
   });
   it("Tests should have randomly generated identifiers", () => {
     expect((new Test()).id).not.toBe((new Test()).id);
