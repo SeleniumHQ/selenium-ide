0667adef44dbf6087b20b18a57e083d5d2bcaf56
Tomer <tomer@corevo.io>
2017-10-18 16:00:46 +0300

    Add and remove tests

--- src/neo/__test__/models/TestCase.spec.js
+++ src/neo/__test__/models/TestCase.spec.js
@@ -19,4 +19,20 @@ describe("TestCase model", () => {
   it("Test Cases should have randomly generated identifiers", () => {
     expect((new TestCase()).id).not.toBe((new TestCase()).id);
   });
+  it("Should have array of commands", () => {
+    expect((new TestCase()).commands).toBeDefined();
+  });
+  it("Should create a command", () => {
+    const test = new TestCase();
+    expect(test.commands.length).toBe(0);
+    test.createCommand();
+    expect(test.commands.length).toBe(1);
+  });
+  it("Should remove a command", () => {
+    const test = new TestCase();
+    const command = test.createCommand();
+    expect(test.commands.length).toBe(1);
+    test.removeCommand(command);
+    expect(test.commands.length).toBe(0);
+  });
 });
