5fd69df71062e852672a0426c8f68d3f18f30c2f
Tomer <tomer@corevo.io>
2017-10-19 11:24:53 +0300

    seed the commands as well

--- src/neo/stores/seed.js
+++ src/neo/stores/seed.js
@@ -1,4 +1,5 @@
 import generate from "project-name-generator";
+import { CommandsArray } from "../models/Command";
 
 export default function seed(store) {
   function generateSuite() {
@@ -7,6 +8,15 @@ export default function seed(store) {
   function generateTestCase() {
     return store.createTestCase(generate({ words: 2}).spaced);
   }
+  const targets = ["a", "button"];
+  function generateCommand(test) {
+    const command = test.createCommand();
+    command.setCommand(CommandsArray[Math.floor(Math.random() * CommandsArray.length)]);
+    let targetChance = Math.floor(Math.random() * 10);
+    command.setTarget(targetChance < targets.length ? targets[targetChance] : "");
+    command.setValue(Math.floor(Math.random() * 2) ? generate({ words: 1}).spaced : "");
+    return command;
+  }
   function randomBetween(min, max) {
     return (Math.floor(Math.random() * (max - min)) + min);
   }
@@ -14,7 +24,11 @@ export default function seed(store) {
   for (let i = 0; i < numberOfSuites; i++) {
     let suite = generateSuite();
     for (let j = 0; j < randomBetween(1, 5); j++) {
-      suite.addTestCase(generateTestCase());
+      const testCase = generateTestCase();
+      for (let k = 0; k < randomBetween(3, 6); k++) {
+        generateCommand(testCase);
+      }
+      suite.addTestCase(testCase);
     }
   }
 
