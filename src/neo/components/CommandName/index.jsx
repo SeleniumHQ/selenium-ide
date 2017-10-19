737470943e6ca3f3c2e0c284a49e89f1ee739c3c
Tomer <tomer@corevo.io>
2017-10-19 18:38:34 +0300

    removed the harsh prop-type check

--- src/neo/components/CommandName/index.jsx
+++ src/neo/components/CommandName/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Commands, CommandsArray } from "../../models/Command";
+import { Commands } from "../../models/Command";
 
 export default class CommandName extends React.Component {
   static propTypes = {
-    children: PropTypes.oneOf(CommandsArray)
+    children: PropTypes.string
   };
   render() {
     return Commands[this.props.children] ? Commands[this.props.children] : this.props.children;
