7c856fbd53b8dd9da59e4c68e03ca307ca6f51ea
Tomer <tomer@corevo.io>
2017-10-22 14:48:58 +0300

    fixed typo

--- src/neo/__test__/models/TestCase.spec.js
+++ src/neo/__test__/models/TestCase.spec.js
@@ -31,21 +31,21 @@ describe("TestCase model", () => {
   });
   it("should throw if the given command is undefined", () => {
     const test = new TestCase();
-    expect(() => test.inserCommandAt()).toThrowError("Expected to receive Command instead received undefined");
+    expect(() => test.insertCommandAt()).toThrowError("Expected to receive Command instead received undefined");
   });
   it("should throw if the given command is different type", () => {
     const test = new TestCase();
-    expect(() => test.inserCommandAt(5)).toThrowError("Expected to receive Command instead received Number");
+    expect(() => test.insertCommandAt(5)).toThrowError("Expected to receive Command instead received Number");
   });
   it("should throw if the given index is undefined", () => {
     const test = new TestCase();
     const command = new Command();
-    expect(() => test.inserCommandAt(command)).toThrowError("Expected to receive Number instead received undefined");
+    expect(() => test.insertCommandAt(command)).toThrowError("Expected to receive Number instead received undefined");
   });
   it("should throw if the given command is different type", () => {
     const test = new TestCase();
     const command = new Command();
-    expect(() => test.inserCommandAt(command, "5")).toThrowError("Expected to receive Number instead received String");
+    expect(() => test.insertCommandAt(command, "5")).toThrowError("Expected to receive Number instead received String");
   });
   it("should insert the command in the middle", () => {
     const test = new TestCase();
