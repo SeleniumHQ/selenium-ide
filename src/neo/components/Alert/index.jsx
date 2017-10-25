02b768ca1831355129e6c83126c26aa331d9afd2
Tomer <tomer@corevo.io>
2017-10-25 16:20:56 +0300

    animate using react transition group

--- src/neo/components/Alert/index.jsx
+++ src/neo/components/Alert/index.jsx
@@ -1,14 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ReactModal from "react-modal";
+import { Transition } from "react-transition-group";
 import FlatButton from "../FlatButton";
 import "./style.css";
+
+const duration = 100;
+
+const transitionStyles = {
+  entering: {
+    opacity: 0,
+    transform: "scale(0, 0)"
+  },
+  entered: {
+    opacity: 1,
+    transform: "scale(1, 1)"
+  },
+  exiting: {
+    opacity: 0,
+    transform: "scale(0, 0)"
+  },
+  exited: {
+    opacity: 0,
+    transform: "scale(0, 0)"
+  }
+};
 
 export default class Alert extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isOpen: true
+      isOpen: false
     };
     this.show = this.show.bind(this);
     this.props.show(this.show);
@@ -30,24 +52,29 @@ export default class Alert extends React.Component {
   }
   render() {
     return (
-      <ReactModal
-        className="alert-content"
-        isOpen={this.state.isOpen}
-        ariaHideApp={false}
-        shouldCloseOnOverlayClick={true}
-        onRequestClose={this.close.bind(this, false)}
-      >
-        <div className="alert">
-          <h2>Night Run</h2>
-          <p>This will permanently delete `Night Run`</p>
-          <hr />
-          <span className="buttons">
-            <FlatButton onClick={this.close.bind(this, false)}>cancel</FlatButton>
-            <FlatButton className="danger" onClick={this.close.bind(this, true)}>delete</FlatButton>
-          </span>
-          <div className="clear"></div>
-        </div>
-      </ReactModal>
+      <Transition in={!!this.state.isOpen} timeout={duration}>
+        {(status) => (
+          <ReactModal
+            className="alert-content"
+            isOpen={this.state.isOpen}
+            ariaHideApp={false}
+            shouldCloseOnOverlayClick={true}
+            closeTimeoutMS={duration}
+            onRequestClose={this.close.bind(this, false)}
+          >
+            <div className="alert" style={transitionStyles[status]}>
+              <h2>Night Run</h2>
+              <p>This will permanently delete `Night Run`</p>
+              <hr />
+              <span className="buttons">
+                <FlatButton onClick={this.close.bind(this, false)}>cancel</FlatButton>
+                <FlatButton className="danger" onClick={this.close.bind(this, true)}>delete</FlatButton>
+              </span>
+              <div className="clear"></div>
+            </div>
+          </ReactModal>
+        )}
+      </Transition>
     );
   }
 }
