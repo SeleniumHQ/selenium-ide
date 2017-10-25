724ee27f71a0d9c632dc9d429816338bd07dc37b
Tomer <tomer@corevo.io>
2017-10-25 14:20:05 +0300

    removed the extra class

--- src/neo/components/FlatButton/index.jsx
+++ src/neo/components/FlatButton/index.jsx
@@ -4,6 +4,6 @@ import "./style.css";
 
 export default class FlatButton extends React.Component {
   render() {
-    return (<button {...this.props} className={classNames("btn", "danger", this.props.className)} />); // eslint-disable-line react/prop-types
+    return (<button {...this.props} className={classNames("btn", this.props.className)} />); // eslint-disable-line react/prop-types
   }
 }
