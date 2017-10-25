c9bb07c9120c78484f10f77f5bbed1a72b83aca8
Tomer <tomer@corevo.io>
2017-10-25 14:19:45 +0300

    moved to a stylesheet

--- src/neo/components/FlatButton/index.jsx
+++ src/neo/components/FlatButton/index.jsx
@@ -1,22 +1,9 @@
-import styled from "styled-components";
+import React from "react";
+import classNames from "classnames";
+import "./style.css";
 
-export default styled.button`
-  background-color: #F5F5F5;
-  padding: 10px 20px;
-  margin: 5px 8px;
-  border-width: 0;
-  border-radius: 4px;
-  outline: 0;
-  text-transform: capitalize;
-  box-shadow: inset -1px 1px 1px 0 transparent;
-  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-
-  &:hover {
-    background-color: #E0E0E0;
+export default class FlatButton extends React.Component {
+  render() {
+    return (<button {...this.props} className={classNames("btn", "danger", this.props.className)} />); // eslint-disable-line react/prop-types
   }
-
-  &:active {
-    background-color: #EEEEEE;
-    box-shadow: inset -1px 1px 1px 0 #CACACA;
-  }
-`;
+}
