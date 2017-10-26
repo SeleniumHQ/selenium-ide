50d44c720d79f4f49ef8b0ccff07d392c4f7f663
Tomer <tomer@corevo.io>
2017-10-26 14:27:20 +0300

    added proptypes

--- src/neo/components/TestSelector/index.jsx
+++ src/neo/components/TestSelector/index.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { PropTypes as MobxPropTypes } from "mobx-react";
 import Modal from "../Modal";
 import RemoveButton from "../ActionButtons/Remove";
 import FlatButton from "../FlatButton";
@@ -14,6 +16,9 @@ export default class TestSelector extends React.Component {
     };
     this.selectTest = this.selectTest.bind(this);
   }
+  static propTypes = {
+    tests: MobxPropTypes.arrayOrObservableArray.isRequired
+  };
   selectTest(isSelected, test) {
     this.setState({
       selectedTests: { ...this.state.selectedTests, [test.id]: isSelected ? test : undefined}
@@ -27,7 +32,7 @@ export default class TestSelector extends React.Component {
           <RemoveButton />
         </span>
         <SearchBar />
-        <TestSelectorList tests={this.props.tests} selectedTest={this.state.selectedTests} selectTest={this.selectTest} />
+        <TestSelectorList tests={this.props.tests} selectedTests={this.state.selectedTests} selectTest={this.selectTest} />
         <hr />
         <span className="right">
           <FlatButton>Cancel</FlatButton>
@@ -42,6 +47,11 @@ export default class TestSelector extends React.Component {
 }
 
 class TestSelectorList extends React.Component {
+  static propTypes = {
+    tests: MobxPropTypes.arrayOrObservableArray.isRequired,
+    selectedTests: PropTypes.object.isRequired,
+    selectTest: PropTypes.func.isRequired
+  };
   handleChange(test, e) {
     this.props.selectTest(e.target.checked, test);
   }
@@ -50,7 +60,7 @@ class TestSelectorList extends React.Component {
       <ul>
         {this.props.tests.map(test => (
           <li key={test.id}>
-            <Checkbox id={test.id} label={test.name} checked={!!this.props.selectedTest[test.id]} onChange={this.handleChange.bind(this, test)} />
+            <Checkbox id={test.id} label={test.name} checked={!!this.props.selectedTests[test.id]} onChange={this.handleChange.bind(this, test)} />
           </li>
         ))}
       </ul>
