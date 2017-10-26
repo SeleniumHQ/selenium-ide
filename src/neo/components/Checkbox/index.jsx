f2236245e5e42234aeaeb5fc852e47f5f8928ba6
Tomer <tomer@corevo.io>
2017-10-26 15:06:09 +0300

    span for the checkbox

--- src/neo/components/Checkbox/index.jsx
+++ src/neo/components/Checkbox/index.jsx
@@ -1,26 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
+import uuidv4 from "uuid/v4";
+import "./style.css";
 
 export default class Checkbox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.id = uuidv4();
+  }
   static propTypes = {
-    id: PropTypes.string.isRequired,
     name: PropTypes.string,
     label: PropTypes.string,
     checked: PropTypes.bool,
     onChange: PropTypes.func.isRequired
   };
   render() {
+    const checked = (this.props.checked || (this.props.hasOwnProperty("checked") && this.props.checked !== false));
     return (
       <div>
         <input
           key="checkbox"
           type="checkbox"
-          id={this.props.id}
+          className="checkbox"
+          id={this.id}
           name={this.props.name}
-          checked={(this.props.checked || (this.props.hasOwnProperty("checked") && this.props.checked !== false))}
+          checked={checked}
           onChange={this.props.onChange}
         />
-        <label key="label" htmlFor={this.props.id}>{this.props.label}</label>
+        <label key="label" htmlFor={this.id}><span>{checked ? "✓" : ""}</span>{this.props.label}</label>
       </div>
     );
   }
