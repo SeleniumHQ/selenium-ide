1530805f605feea784da9c5ac53fbe35e4d27c25
Tomer <tomer@corevo.io>
2017-10-26 13:51:49 +0300

    added all the proptypes

--- src/neo/components/Checkbox/index.jsx
+++ src/neo/components/Checkbox/index.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export default class Checkbox extends React.Component {
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string,
+    label: PropTypes.string,
+    checked: PropTypes.bool,
+    handleChange: PropTypes.func.isRequired
+  };
   render() {
     return (
       <div>
-        <input key="checkbox" type="checkbox" id={this.props.id} name={this.props.name} />
+        <input
+          key="checkbox"
+          type="checkbox"
+          id={this.props.id}
+          name={this.props.name}
+          checked={(this.props.checked || (this.props.hasOwnProperty("checked") && this.props.checked !== false))}
+          onChange={this.props.handleChange}
+        />
         <label key="label" htmlFor={this.props.id}>{this.props.label}</label>
       </div>
     );
