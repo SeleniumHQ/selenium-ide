6099e9cb14adc613596c7fdad63a0b56ef287af8
Tomer <tomer@corevo.io>
2017-10-30 17:19:24 +0200

    moved the url bar to the editor

--- src/neo/components/UrlBar/index.jsx
+++ src/neo/components/UrlBar/index.jsx
@@ -4,10 +4,9 @@ import "./style.css";
 export default class UrlBar extends React.Component {
   render() {
     return (
-      <span className="url">
-        <label htmlFor="url">Base URL</label>
-        <input id="url" name="url" type="text" placeholder="http://www.seleniumhq.org" />
-      </span>
+      <div className="url">
+        <input id="url" name="url" type="url" placeholder="http://www.seleniumhq.org" />
+      </div>
     );
   }
 }
--- src/neo/containers/Editor/index.jsx
+++ src/neo/containers/Editor/index.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { observer } from "mobx-react";
 import UiState from "../../stores/view/UiState";
 import ToolBar from "../../components/ToolBar";
+import UrlBar from "../../components/UrlBar";
 import TestTable from "../../components/TestTable";
 import CommandForm from "../../components/CommandForm";
 
@@ -37,6 +38,7 @@ import CommandForm from "../../components/CommandForm";
         display: "flow-root"
       }}>
         <ToolBar />
+        <UrlBar />
         <TestTable
           commands={this.props.test ? this.props.test.commands : null}
           selectedCommand={UiState.selectedCommand ? UiState.selectedCommand.id : null}
