8a7fc290723011f56845fc842a080720b1624f37
Tomer <tomer.steinfeld@applitools.com>
2017-11-06 15:24:39 +0200

    play button toggles playback

--- src/neo/components/ToolBar/index.jsx
+++ src/neo/components/ToolBar/index.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import { observer } from "mobx-react";
 import PlayAll from "../../components/ActionButtons/PlayAll";
-import PlayCurrent from "../../components/ActionButtons/PlayCurrent";
 import Pause from "../../components/ActionButtons/Pause";
 import StepInto from "../../components/ActionButtons/StepInto";
 import SpeedGauge from "../../components/ActionButtons/SpeedGauge";
 import Record from "../../components/ActionButtons/Record";
 import UiState from "../../stores/view/UiState";
+import PlaybackState from "../../stores/view/PlaybackState";
 
 @observer
 export default class ToolBar extends React.Component {
   render() {
     return (
       <span>
-        <PlayAll />
-        <PlayCurrent />
+        <PlayAll onClick={PlaybackState.togglePlaying} />
         <Pause />
         <StepInto />
         <SpeedGauge />
