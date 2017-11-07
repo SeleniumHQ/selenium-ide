4930f7a9b9a1f2866d94a2fb581b8cc77f7f8993
Tomer <tomer.steinfeld@applitools.com>
2017-11-07 10:50:36 +0200

    only logs for now

--- src/neo/containers/Console/index.jsx
+++ src/neo/containers/Console/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import TabBar from "../../components/TabBar";
+import LogList from "../../components/LogList";
 import "./style.css";
 
 export default class Console extends React.Component {
   render() {
     return (
       <footer className="console">
-        <TabBar tabs={["Log", "Reference", "UI-Element", "Rollup"]} tabWidth={110} />
+        <TabBar tabs={["Log"]} tabWidth={70} />
+        <LogList />
       </footer>
     );
   }
