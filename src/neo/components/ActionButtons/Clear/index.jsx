3b33a0cc074aa39818d5071b94bd8bcc506fb6d3
Tomer <tomer.steinfeld@applitools.com>
2017-11-07 15:04:01 +0200

    Added the clear button

---
+++ src/neo/components/ActionButtons/Clear/index.jsx
@@ -0,0 +1,6 @@
+import ActionButton from "../ActionButton";
+import clear from "../../../images/ic_clear.svg";
+
+export default ActionButton.extend`
+  mask-image: url(${clear});
+`;
--- src/neo/containers/Console/index.jsx
+++ src/neo/containers/Console/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import TabBar from "../../components/TabBar";
 import LogList from "../../components/LogList";
+import ClearButton from "../../components/ActionButtons/Clear";
 import "./style.css";
 
 export default class Console extends React.Component {
   render() {
     return (
       <footer className="console">
-        <TabBar tabs={["Log"]} tabWidth={70} />
+        <TabBar tabs={["Log"]} tabWidth={70}>
+          <ClearButton />
+        </TabBar>
         <LogList />
       </footer>
     );
