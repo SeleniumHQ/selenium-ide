88c313bc4ae7b458fdeb7a75ced0510886b6b5ce
Tomer <tomer.steinfeld@applitools.com>
2017-11-09 14:00:31 +0200

    use the style

--- src/neo/components/ActionButtons/ActionButton/index.jsx
+++ src/neo/components/ActionButtons/ActionButton/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
+import "./style.css";
 
 export default class ActionButton extends React.Component {
   render() {
