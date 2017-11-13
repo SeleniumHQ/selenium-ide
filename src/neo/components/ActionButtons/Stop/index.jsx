f6c33137273234abeec81224782e99744865ea28
Tomer <tomer.steinfeld@applitools.com>
2017-11-13 17:05:56 +0200

    added the stop button

---
+++ src/neo/components/ActionButtons/Stop/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ActionButton from "../ActionButton";
+import classNames from "classnames";
+
+export default class PlayCurrentButton extends React.Component {
+  render() {
+    return (
+      <ActionButton {...this.props} className={classNames("si-stop", this.props.className)} />// eslint-disable-line react/prop-types
+    );
+  }
+}
