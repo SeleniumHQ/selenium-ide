bf39bd5739a25ca65ad27ec7ca812903f0bc0cb1
Tomer <tomer.steinfeld@applitools.com>
2017-11-14 15:35:27 +0200

    bottom direction

--- src/neo/components/GaugeMenu/index.jsx
+++ src/neo/components/GaugeMenu/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Slider from "rc-slider";
-import Menu from "../Menu";
+import Menu, { MenuDirections } from "../Menu";
 import "rc-slider/assets/index.css";
 import "./style.css";
 
@@ -11,7 +11,7 @@ export default class GaugeMenu extends React.Component {
   };
   render() {
     return (
-      <Menu opener={this.props.opener} closeOnClick={false} width={50} padding={0}>
+      <Menu opener={this.props.opener} direction={MenuDirections.Bottom} closeOnClick={false} width={40} padding={0}>
         <div className="speed-gauge">
           <span>Fast</span>
           <Slider vertical included={false} defaultValue={50} onChange={console.log.bind(console)} />
