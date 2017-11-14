9260cf6e7bafb96b395b23207b7d253acb8a319f
Tomer <tomer.steinfeld@applitools.com>
2017-11-14 15:51:43 +0200

    caret styling

--- src/neo/components/ActionButtons/SpeedGauge/index.jsx
+++ src/neo/components/ActionButtons/SpeedGauge/index.jsx
@@ -1,11 +1,15 @@
+/*eslint-disable react/prop-types*/
 import React from "react";
 import ActionButton from "../ActionButton";
 import classNames from "classnames";
+import "./style.css";
 
 export default class SpeedGaugeButton extends React.Component {
   render() {
     return (
-      <ActionButton {...this.props} className={classNames("si-gauge", this.props.className)} />// eslint-disable-line react/prop-types
+      <ActionButton {...this.props} className={classNames("si-gauge", this.props.className)}>
+        <i className="si-caret" />
+      </ActionButton>
     );
   }
 }
