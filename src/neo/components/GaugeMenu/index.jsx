a4d1c6611d8403662a84c4f51b5f3ff7620420d8
Tomer <tomer.steinfeld@applitools.com>
2017-11-14 16:23:32 +0200

    added all of the required properties

--- src/neo/components/GaugeMenu/index.jsx
+++ src/neo/components/GaugeMenu/index.jsx
@@ -7,14 +7,20 @@ import "./style.css";
 
 export default class GaugeMenu extends React.Component {
   static propTypes = {
-    opener: PropTypes.element
+    opener: PropTypes.element,
+    value: PropTypes.number.isRequired,
+    maxDelay: PropTypes.number.isRequired,
+    onChange: PropTypes.func.isRequired
   };
+  handleChange(value) {
+    this.props.onChange(this.props.maxDelay - value);
+  }
   render() {
     return (
       <Menu opener={this.props.opener} direction={MenuDirections.Bottom} closeOnClick={false} width={40} padding={0}>
         <div className="speed-gauge">
           <span>Fast</span>
-          <Slider vertical included={false} defaultValue={50} onChange={console.log.bind(console)} />
+          <Slider vertical included={false} min={0} max={this.props.maxDelay} value={this.props.maxDelay - this.props.value} onChange={this.handleChange.bind(this)} />
           <span>Slow</span>
         </div>
       </Menu>
