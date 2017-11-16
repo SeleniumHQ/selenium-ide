817ad6cdb841f7bad2d2039e85667d0ab81d03f6
Tomer <tomer.steinfeld@applitools.com>
2017-11-16 11:07:17 +0200

    dont log browser errors in production

--- src/setup.js
+++ src/setup.js
@@ -3,4 +3,11 @@
  */
 import browser from "webextension-polyfill";
 
+const emptyFunc = new Function();
+function log(func, ...argv) {
+  func(...argv).catch(process.env.NODE_ENV !== "production" ? console.log.bind(console) : emptyFunc);
+}
+
+browser.runtime.sendMessage = log.bind(undefined, browser.runtime.sendMessage);
+
 window.browser = browser;
