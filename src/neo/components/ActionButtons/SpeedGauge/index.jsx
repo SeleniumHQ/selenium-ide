37dcaf5d1299c93bc11fca6fb86224afbfdc7f90
Tomer <tomer.steinfeld@applitools.com>
2017-11-20 10:37:19 +0200

    default gauge speed

--- src/neo/components/ActionButtons/SpeedGauge/index.jsx
+++ src/neo/components/ActionButtons/SpeedGauge/index.jsx
@@ -1,13 +1,20 @@
 /*eslint-disable react/prop-types*/
 import React from "react";
+import PropTypes from "prop-types";
 import ActionButton from "../ActionButton";
 import classNames from "classnames";
 import "./style.css";
 
 export default class SpeedGaugeButton extends React.Component {
+  static propTypes = {
+    speed: PropTypes.number
+  };
+  static defaultProps = {
+    speed: 6
+  };
   render() {
     return (
-      <ActionButton {...this.props} className={classNames("si-gauge", this.props.className)}>
+      <ActionButton {...this.props} className={classNames(`si-gauge-${this.props.speed}`, this.props.className)}>
         <i className="si-caret" />
       </ActionButton>
     );
