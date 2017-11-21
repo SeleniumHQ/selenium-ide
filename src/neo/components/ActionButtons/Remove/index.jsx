cdcfe01fd3b52752f6e1f2e158127c6cd387fa98
Tomer <tomer.steinfeld@applitools.com>
2017-11-21 16:30:02 +0200

    dont focus the remove buttons

--- src/neo/components/ActionButtons/Remove/index.jsx
+++ src/neo/components/ActionButtons/Remove/index.jsx
@@ -5,7 +5,7 @@ import classNames from "classnames";
 export default class RemoveButton extends React.Component {
   render() {
     return (
-      <ActionButton {...this.props} className={classNames("si-remove", this.props.className)} />// eslint-disable-line react/prop-types
+      <ActionButton tabIndex="-1" {...this.props} className={classNames("si-remove", this.props.className)} />// eslint-disable-line react/prop-types
     );
   }
 }
