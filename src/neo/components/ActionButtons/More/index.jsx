edbfe625eec093805f679f399a7f56610d3bdf0c
Tomer <tomer.steinfeld@applitools.com>
2017-11-21 16:34:34 +0200

    no focus styling on remove and more

--- src/neo/components/ActionButtons/More/index.jsx
+++ src/neo/components/ActionButtons/More/index.jsx
@@ -5,7 +5,7 @@ import classNames from "classnames";
 export default class MoreButton extends React.Component {
   render() {
     return (
-      <ActionButton tabIndex="-1" {...this.props} className={classNames("si-more", this.props.className)} />// eslint-disable-line react/prop-types
+      <ActionButton tabIndex="-1" {...this.props} className={classNames("no-focus", "si-more", this.props.className)} />// eslint-disable-line react/prop-types
     );
   }
 }
--- src/neo/components/ActionButtons/Remove/index.jsx
+++ src/neo/components/ActionButtons/Remove/index.jsx
@@ -5,7 +5,7 @@ import classNames from "classnames";
 export default class RemoveButton extends React.Component {
   render() {
     return (
-      <ActionButton tabIndex="-1" {...this.props} className={classNames("si-remove", this.props.className)} />// eslint-disable-line react/prop-types
+      <ActionButton tabIndex="-1" {...this.props} className={classNames("no-focus", "si-remove", this.props.className)} />// eslint-disable-line react/prop-types
     );
   }
 }
