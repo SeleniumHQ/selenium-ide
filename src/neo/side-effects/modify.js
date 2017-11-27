be84d73185201b353af5e2799c368606add98cfa
Tomer <tomer.steinfeld@applitools.com>
2017-11-27 17:40:04 +0200

    improve changes detection

--- src/neo/side-effects/modify.js
+++ src/neo/side-effects/modify.js
@@ -2,7 +2,7 @@ import { spy } from "mobx";
 
 export default function(project) {
   let disposer = spy((event) => {
-    if (!project.modified && event.object && event.object.constructor.name !== "UiState" && event.type === "action" && event.name !== "setModified") {
+    if (!project.modified && event.object && event.object.constructor.name === "ProjectStore" && event.type === "action" && event.name !== "setModified") {
       project.setModified();
     } else if (project.modified) {
       setTimeout(disposer, 0);
