2e1c6ed5780f309655053a748a1e842165b62dfa
Tomer <tomer.steinfeld@applitools.com>
2017-11-29 12:13:52 +0200

    allow getting the button ref

--- src/neo/components/FlatButton/index.jsx
+++ src/neo/components/FlatButton/index.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 import classNames from "classnames";
 import "./style.css";
 
 export default class FlatButton extends React.Component {
+  static propTypes = {
+    buttonRef: PropTypes.func
+  };
   render() {
-    return (<button type="button" {...this.props} className={classNames("btn", this.props.className)} />); // eslint-disable-line react/prop-types
+    const props = {...this.props};
+    delete props.buttonRef;
+    return (<button type="button" ref={this.props.buttonRef} {...props} className={classNames("btn", this.props.className)} />); // eslint-disable-line react/prop-types
   }
 }
