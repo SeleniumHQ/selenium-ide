d84581a28d448cbe24e37ae3d2c80174aa62dd13
Tomer <tomer.steinfeld@applitools.com>
2017-11-30 10:21:36 +0200

    add support for default tab

--- src/neo/components/VerticalTabBar/index.jsx
+++ src/neo/components/VerticalTabBar/index.jsx
@@ -3,6 +3,16 @@ import PropTypes from "prop-types";
 import "./style.css";
 
 export default class VerticalTabbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: props.defaultTab ? {
+        tab: props.defaultTab
+      } : {
+        tab: props.tabs[0]
+      }
+    };
+  }
   static propTypes = {
     children: PropTypes.node,
     tabs: PropTypes.array.isRequired,
