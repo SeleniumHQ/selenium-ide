8e0c49f244ac278f9bd699403b8b64c17d078800
Tomer <tomer.steinfeld@applitools.com>
2017-11-30 16:38:08 +0200

    added :focus the the record button

--- src/neo/components/ActionButtons/Record/index.jsx
+++ src/neo/components/ActionButtons/Record/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import "./style.css";
 
 const RecordButton = styled.button`
   height: 16px;
   width: 16px;
-  margin: 6px 10px 6px 5px;
+  margin: 3px 4px;
   background-color: #EE4841;
   border-style: none;
   border-radius: ${props => props.isActive ? "10%" : "50%"};
@@ -67,9 +68,9 @@ export default class Record extends React.Component {
   };
   render() {
     return (
-      <span className="record" data-tip={this.props.isRecording ? "<p>Stop recording</p>" : "<p>Start recording</p>"}>
+      <div className="record" data-tip={this.props.isRecording ? "<p>Stop recording</p>" : "<p>Start recording</p>"}>
         <RecordButton isActive={this.props.isRecording} onClick={this.props.onClick}></RecordButton>
-      </span>
+      </div>
     );
   }
 }
