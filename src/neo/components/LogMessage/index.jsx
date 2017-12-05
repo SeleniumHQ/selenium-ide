7ad52d0ce35cf29703d3a9e2ff923edad1527b19
Tomer <tomer.steinfeld@applitools.com>
2017-12-05 11:23:18 +0200

    added index

--- src/neo/components/LogMessage/index.jsx
+++ src/neo/components/LogMessage/index.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { observer } from "mobx-react";
+import classNames from "classnames";
 import "./style.css";
 
 @observer
 export default class LogMessage extends React.Component {
   render() {
     return (
-      <li className={this.props.log.status}>{this.props.log.message}</li>
+      <li className={classNames("log", this.props.log.status)}>
+        {this.props.log.index && <span>{this.props.log.index}.</span>}
+        {this.props.log.message}
+      </li>
     );
   }
   static propTypes = {
