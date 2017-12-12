de23711d26dd36be9020d59761b17da60f817236
Tomer <tomer.steinfeld@applitools.com>
2017-12-12 13:23:01 +0200

    remove unused uglify plugin call

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -250,24 +250,6 @@ export default {
         "NODE_ENV": JSON.stringify(process.env.NODE_ENV)
       }
     }),
-    // Minify the code.
-    /*new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        // Disabled because of an issue with Uglify breaking seemingly valid code:
-        // https://github.com/facebookincubator/create-react-app/issues/2376
-        // Pending further investigation:
-        // https://github.com/mishoo/UglifyJS2/issues/2011
-        comparisons: false
-      },
-      output: {
-        comments: false,
-        // Turned on because emoji and regex is not minified properly using default
-        // https://github.com/facebookincubator/create-react-app/issues/2488
-        ascii_only: true
-      },
-      sourceMap: isProduction
-    }),*/
     // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
     new ExtractTextPlugin({
       filename: "css/[name].[hash:8].css"
