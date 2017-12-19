e5ae29cdb8bdaa59fc5dc523c6d9126138b0a20e
Tomer <tomer.steinfeld@applitools.com>
2017-12-19 15:48:22 +0200

    command emitting failures

---
+++ src/selianize/command.js
@@ -0,0 +1,33 @@
+// Licensed to the Software Freedom Conservancy (SFC) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The SFC licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+const emitters = {};
+
+export function emit(command) {
+  return new Promise(async (res, rej) => {
+    if (emitters[command.command]) {
+      let result = await emitters[command.command]({...command});
+      res(result);
+    } else {
+      rej(command.command ? `Unknown command ${command.command}` : "Command can not be empty");
+    }
+  });
+}
+
+export default {
+  emit
+};
