ca05a7407c36feb1558bee6e8e483225bd460ff2
Tomer <tomer.steinfeld@applitools.com>
2018-03-15 11:29:01 +0200

    add it to the build

--- tests/simple-export.js
+++ tests/simple-export.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 var fs = require("fs");
 var path = require("path");
-var selianize = require("./selianize").default;
+var selianize = require("../packages/selianize").default;
 var list = process.argv.slice(2);
 list.forEach(filePath => {
   var file = JSON.parse(fs.readFileSync(path.join("examples", filePath)).toString());
