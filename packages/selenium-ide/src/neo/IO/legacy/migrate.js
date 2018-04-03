8e6d9a783007737a3dbeec134d144649398d3ad5
Tomer <tomer.steinfeld@applitools.com>
2018-04-03 14:38:12 +0300

    remove empty commands when migrating

--- packages/selenium-ide/src/neo/IO/legacy/migrate.js
+++ packages/selenium-ide/src/neo/IO/legacy/migrate.js
@@ -100,9 +100,9 @@ export function migrateTestCase(data) {
       {
         id: data,
         name: result.html.body.table.thead.tr.td._text,
-        commands: result.html.body.table.tbody.tr.filter(row => !/^wait/.test(row.td[0]._text)).map(row => (
+        commands: result.html.body.table.tbody.tr.filter(row => (row.td[0]._text && !/^wait/.test(row.td[0]._text))).map(row => (
           {
-            command: row.td[0]._text && row.td[0]._text.replace("AndWait", "") || "",
+            command: row.td[0]._text && row.td[0]._text.replace("AndWait", ""),
             target: xmlunescape(parseTarget(row.td[1])),
             value: xmlunescape(row.td[2]._text || "")
           }
