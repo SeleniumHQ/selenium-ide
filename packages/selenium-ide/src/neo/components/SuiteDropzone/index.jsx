469be3218a6a0d6cee6881a3f5b19f7571116034
Tomer <tomer.steinfeld@applitools.com>
2018-04-26 13:38:17 +0300

    remove dropzone to speedup the table dnd

--- packages/selenium-ide/src/neo/components/SuiteDropzone/index.jsx
+++ packages/selenium-ide/src/neo/components/SuiteDropzone/index.jsx
@@ -1,20 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Dropzone from "react-dropzone";
+import { DropTarget } from "react-dnd";
+import { NativeTypes } from "react-dnd-html5-backend";
 
+const boxTarget = {
+  drop(props, monitor) {
+    const files = monitor.getItem().files;
+    if (props.loadProject && files.length && /\.side$/.test(files[0].name)) {
+      props.loadProject(files[0]);
+    }
+  }
+};
+
+@DropTarget(NativeTypes.FILE, boxTarget, (connect, monitor) => ({
+  connectDropTarget: connect.dropTarget(),
+  isOver: monitor.isOver(),
+  canDrop: monitor.canDrop()
+}))
 export default class SuiteDropzone extends React.Component {
   static propTypes = {
     children: PropTypes.node,
-    loadProject: PropTypes.func
+    loadProject: PropTypes.func,
+    connectDropTarget: PropTypes.func.isRequired
   };
-  onDrop(blobs) {
-    this.props.loadProject(blobs[0]);
-  }
   render() {
-    return (
-      <Dropzone className="suite-dropzone" acceptClassName="accept" accept=".side" disableClick={true} multiple={false} onDropAccepted={this.onDrop.bind(this)}>
-        {this.props.children}
-      </Dropzone>
+    return this.props.connectDropTarget(
+      <div>{this.props.children}</div>
     );
   }
 }
