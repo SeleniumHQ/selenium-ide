9183259db92c962231b6e496ac204d18e7c6beac
Tomer <tomer.steinfeld@applitools.com>
2018-04-30 14:25:31 +0300

    handle undefines

--- packages/selianize/src/testcase.js
+++ packages/selianize/src/testcase.js
@@ -24,7 +24,7 @@ export function emit(test) {
     const hookResults = await Promise.all(hooks.map((hook) => hook(test)));
 
     let result = `it("${test.name}", () => {`;
-    result += hookResults.map((hook) => hook.setup).join("");
+    result += hookResults.map((hook) => hook.setup || "").join("");
 
     let errors = [];
     result += (await Promise.all(test.commands.map((command, index) => (CommandEmitter.emit(command).catch(e => {
@@ -35,8 +35,8 @@ export function emit(test) {
       });
     }))))).join("");
 
+    result += hookResults.map((hook) => hook.teardown || "").join("");
     result += "return driver.getTitle().then(title => {expect(title).toBeDefined();});});";
-    result += hookResults.map((hook) => hook.teardown).join("");
 
     errors.length ? rej({...test, commands: errors}) : res(result);
   });
