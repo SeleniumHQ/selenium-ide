3b546cd4cd8f90d2d982b259e2313c9e91fbbdca
David Haeffner <dhaeffner@gmail.com>
2018-06-14 11:21:46 -0400

    Added placeholders for tests that need to be added.

--- packages/selenium-ide/src/neo/__test__/components/TabBar/index.spec.js
+++ packages/selenium-ide/src/neo/__test__/components/TabBar/index.spec.js
@@ -1,18 +1,22 @@
 import React from "react";
-import {renderIntoDocument, fireEvent, cleanup} from "react-testing-library";
-import TabBar from "../../../components/TabBar"
+import { renderIntoDocument, fireEvent, cleanup } from "react-testing-library";
+import TabBar from "../../../components/TabBar";
 
 describe("<TabBar />", () => {
   afterEach(cleanup);
+  xit("should show an unread notification when the log updates", () => {
+  });
+  xit("should not show an unread notification when the log updates and viewing the log tab", () => {
+  });
   it("should return the selected tab name when selected", () => {
     const handleTabChange = (passedSelectedTab) => {
       expect(passedSelectedTab).toBe("Reference");
-    }
-    const { container } = renderIntoDocument(<TabBar tabs={["Log", "Reference"]} tabChanged={handleTabChange}/>);
+    };
+    const { container } = renderIntoDocument(<TabBar tabs={[{ name: "Log" }, { name: "Reference" }]} tabChanged={handleTabChange}/>);
     const referenceTab = container.querySelector(".reference");
-    fireEvent(referenceTab, new MouseEvent('click', {
+    fireEvent(referenceTab, new MouseEvent("click", {
       bubbles: true,
-      cancelable: true,
+      cancelable: true
     }));
   });
 });
---
+++ packages/selenium-ide/src/neo/__test__/containers/Console/index.spec.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { renderIntoDocument, fireEvent, cleanup } from "react-testing-library";
+import TabBar from "../../../containers/Console";
+
+xdescribe("<Console />", () => {
+  afterEach(cleanup);
+  xit("should render", () => {
+  });
+});
