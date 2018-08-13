fae5f7ec754e8ff399f27b54e771d3718eaf6a6e
Tomer <tomer@corevo.io>
2018-08-13 15:24:09 +0300

    separated the browser webdriver test suite into it's own

--- packages/webdriver/test/index.spec.js
+++ packages/webdriver/test/index.spec.js
@@ -15,21 +15,31 @@
 // specific language governing permissions and limitations
 // under the License.
 
+import fs from "fs";
+import path from "path";
 import webdriver from "selenium-webdriver";
 
+const SERVER = process.env.SELENIUM_SERVER || "http://localhost:4444/wd/hub";
+
 describe("browser webdriver", () => {
+  beforeAll(() => {
+    if (!fs.existsSync(path.join(__dirname, "../build/webdriver.js")))
+      throw new Error("Please run `yarn build:webdriver` or `yarn build:webdriver:dev` prior to running tests!");
+  });
   it("should get web page title", async () => {
-    const driver = new webdriver.Builder().forBrowser("chrome").usingServer("http://localhost:4444/wd/hub").build();
-    driver.get("http://nginx/webdriver.html");
+    const driver = new webdriver.Builder().forBrowser("chrome").usingServer(SERVER).build();
+    const clientScript = fs.readFileSync(path.join(__dirname, "../build/webdriver.js")).toString();
+    await driver.executeScript(clientScript);
 
     let result = await driver.executeAsyncScript((...args) => {
+      const SERVER = args[0];
       let done = args[args.length - 1];
-      let d = new window.browserWebdriver.Builder().forBrowser("chrome").usingServer("http://localhost:4444/wd/hub").build();
+      let d = new window.browserWebdriver.Builder().forBrowser("chrome").usingServer(SERVER).build();
       d.get("https://google.com");
       d.getTitle().then(title => {
         done(title);
       });
-    });
+    }, SERVER);
 
     expect(result).toBe("Google");
   });
