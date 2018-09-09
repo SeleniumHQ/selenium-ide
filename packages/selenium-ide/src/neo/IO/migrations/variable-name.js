f24c8acfcca8410e034e2fc669a24bb914a579d5
David Haeffner <dhaeffner@gmail.com>
2018-09-09 08:47:40 -0400

    Added migration to convert ${varName} to varName for commands that use the variable name ArgType (re: #382)

---
+++ packages/selenium-ide/src/neo/IO/migrations/variable-name.js
@@ -0,0 +1,47 @@
+// Licensed to the Software Freedom Conservancy (SFC) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The SFC licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import { Commands, ArgTypes } from "../../models/Command";
+
+export default function migrate(project) {
+  let r = Object.assign({}, project);
+  r.tests = r.tests.map((test) => {
+    return Object.assign({}, test, {
+      commands: test.commands.map((c) => {
+        if (Commands.list.has(c.command)) {
+          let newCmd = Object.assign({}, c);
+          const type = Commands.list.get(c.command);
+          if (type.target && type.target.name === ArgTypes.variableName.name) {
+            newCmd.target = migrateVariableName(newCmd.target);
+          }
+          if (type.value && type.value.name === ArgTypes.variableName.name) {
+            newCmd.value = migrateVariableName(newCmd.value);
+          }
+          return newCmd;
+        }
+        return c;
+      })
+    });
+  });
+  return r;
+}
+
+function migrateVariableName(target) {
+  return target.replace(/\$\{(\w+)\}/g, "$1");
+}
+
+migrate.version = "1.1";
---
+++ packages/selenium-ide/src/neo/__test__/IO/migrations/variable-name.spec.js
@@ -0,0 +1,65 @@
+// Licensed to the Software Freedom Conservancy (SFC) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The SFC licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import migrate from "../../../IO/migrations/variable-name";
+
+describe("project migrator", () => {
+  it("should be included in 1.1", () => {
+    expect(migrate.version).toBe("1.1");
+  });
+  it("should migrate variable reference by interpolation into variable reference by name", () => {
+    const project = {
+      tests: [
+        {
+          id: "1",
+          commands: [
+            {
+              command: "assert",
+              target: "${x}",
+              value: ""
+            },
+            {
+              command: "verify",
+              target: "${x}",
+              value: ""
+            }
+          ]
+        }
+      ]
+    };
+    const upgraded = migrate(project);
+    expect(upgraded).toEqual({
+      tests: [
+        {
+          id: "1",
+          commands: [
+            {
+              command: "assert",
+              target: "x",
+              value: ""
+            },
+            {
+              command: "verify",
+              target: "x",
+              value: ""
+            }
+          ]
+        }
+      ]
+    });
+  });
+});
