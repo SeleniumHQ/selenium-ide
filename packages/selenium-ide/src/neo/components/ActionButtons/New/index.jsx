d0e3420a0ca9c5183c2e6cfbbad906a44c966194
Tomer <tomer@corevo.io>
2018-09-30 12:26:25 +0300

    fixed new project icon

--- packages/selenium-ide/src/neo/components/ActionButtons/New/index.jsx
+++ packages/selenium-ide/src/neo/components/ActionButtons/New/index.jsx
@@ -27,7 +27,7 @@ export default class NewButton extends React.Component {
       <ActionButton
         data-tip={`<p>Create new project <span style="color: #929292;padding-left: 5px;">${parse("n", { primaryKey: true, shiftKey: true })}</span></p>`}
         {...props}
-        className={classNames("si-new", this.props.className)} />// eslint-disable-line react/prop-types
+        className={classNames("si-new-project", this.props.className)} />// eslint-disable-line react/prop-types
     );
   }
 }
