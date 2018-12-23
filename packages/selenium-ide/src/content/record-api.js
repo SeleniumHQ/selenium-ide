746eb881d1096b2b5f28b2272ffe510aeedbb68e
David Haeffner <dhaeffner@gmail.com>
2018-12-23 10:18:07 +0200

    Added fix for recording frame indices when a frame gets deleted

--- packages/selenium-ide/src/content/record-api.js
+++ packages/selenium-ide/src/content/record-api.js
@@ -175,58 +175,56 @@ browser.runtime.onMessage.addListener(attachRecorderHandler)
 browser.runtime.onMessage.addListener(detachRecorderHandler)
 
 function addRecordingIndicator() {
-  if (!recordingIndicator && frameLocation === 'root') {
-    browser.runtime
+  if (frameLocation === 'root' && !recordingIndicator) {
+    const indicatorIndex = window.parent.frames.length
+    recordingIndicator = window.document.createElement('iframe')
+    recordingIndicator.src = browser.runtime.getURL('/indicator.html')
+    recordingIndicator.id = 'selenium-ide-indicator'
+    recordingIndicator.style.border = '1px solid white'
+    recordingIndicator.style.position = 'fixed'
+    recordingIndicator.style.bottom = '36px'
+    recordingIndicator.style.right = '36px'
+    recordingIndicator.style.width = '280px'
+    recordingIndicator.style.height = '80px'
+    recordingIndicator.style['background-color'] = 'whitesmoke'
+    recordingIndicator.style['box-shadow'] = '7px 7px 10px 0 rgba(0,0,0,0.3)'
+    recordingIndicator.style.transition = 'bottom 100ms linear'
+    recordingIndicator.style['z-index'] = 1000000000000000
+    recordingIndicator.addEventListener(
+      'mouseenter',
+      function(event) {
+        event.target.style.visibility = 'hidden'
+        setTimeout(function() {
+          event.target.style.visibility = 'visible'
+        }, 1000)
+      },
+      false
+    )
+    window.document.body.appendChild(recordingIndicator)
+    browser.runtime.onMessage.addListener(function(
+      message,
+      sender, // eslint-disable-line
+      sendResponse
+    ) {
+      if (message.recordNotification) {
+        recordingIndicator.contentWindow.postMessage(
+          {
+            direction: 'from-recording-module',
+            command: message.command,
+            target: message.target,
+            value: message.value,
+          },
+          '*'
+        )
+        sendResponse(true)
+      }
+    })
+    return browser.runtime
       .sendMessage({
         setFrameNumberForTab: true,
-        indicatorIndex: window.parent.frames.length,
-        length: window.parent.frames.length + 1,
+        indicatorIndex: indicatorIndex,
       })
-      .then(() => {
-        recordingIndicator = window.document.createElement('iframe')
-        recordingIndicator.src = browser.runtime.getURL('/indicator.html')
-        recordingIndicator.id = 'selenium-ide-indicator'
-        recordingIndicator.style.border = '1px solid white'
-        recordingIndicator.style.position = 'fixed'
-        recordingIndicator.style.bottom = '36px'
-        recordingIndicator.style.right = '36px'
-        recordingIndicator.style.width = '280px'
-        recordingIndicator.style.height = '80px'
-        recordingIndicator.style['background-color'] = 'whitesmoke'
-        recordingIndicator.style['box-shadow'] =
-          '7px 7px 10px 0 rgba(0,0,0,0.3)'
-        recordingIndicator.style.transition = 'bottom 100ms linear'
-        recordingIndicator.style['z-index'] = 1000000000000000
-        recordingIndicator.addEventListener(
-          'mouseenter',
-          function(event) {
-            event.target.style.visibility = 'hidden'
-            setTimeout(function() {
-              event.target.style.visibility = 'visible'
-            }, 1000)
-          },
-          false
-        )
-        window.document.body.appendChild(recordingIndicator)
-        browser.runtime.onMessage.addListener(function(
-          message,
-          sender, // eslint-disable-line
-          sendResponse
-        ) {
-          if (message.recordNotification) {
-            recordingIndicator.contentWindow.postMessage(
-              {
-                direction: 'from-recording-module',
-                command: message.command,
-                target: message.target,
-                value: message.value,
-              },
-              '*'
-            )
-            sendResponse(true)
-          }
-        })
-      })
+      .catch(() => {})
   }
 }
 
@@ -237,13 +235,14 @@ function getFrameCount() {
 }
 
 function removeRecordingIndicator() {
-  if (recordingIndicator)
+  if (frameLocation === 'root' && recordingIndicator) {
     recordingIndicator.parentElement.removeChild(recordingIndicator)
+    recordingIndicator = undefined
+  }
 }
 
-// TODO: Decouple frame location from recording since its also used in playback
 // set frame id
-async function getframeLocation() {
+async function getFrameLocation() {
   let currentWindow = window
   let currentParentWindow
   let recordingIndicatorIndex
@@ -275,13 +274,28 @@ async function getframeLocation() {
     }
   }
   frameLocation = 'root' + frameLocation
+  browser.runtime.sendMessage({ frameLocation: frameLocation }).catch(() => {})
 }
 
-;(() => {
-  getframeLocation()
-})()
+async function recalculateFrameLocation(message, _sender, sendResponse) {
+  if (message.recalculateFrameLocation) {
+    removeRecordingIndicator()
+    setTimeout(async () => {
+      await addRecordingIndicator()
+    }, 100)
+    frameLocation = ''
+    await getFrameLocation()
+    sendResponse(true)
+  }
+}
 
-browser.runtime.sendMessage({ frameLocation: frameLocation }).catch(() => {})
+browser.runtime.onMessage.addListener(recalculateFrameLocation)
+
+// runs in the content script of each frame
+// e.g., once on load
+;(async () => {
+  await getFrameLocation()
+})()
 
 const recorder = new Recorder(window)
 window.recorder = recorder
--- packages/selenium-ide/src/neo/IO/SideeX/recorder.js
+++ packages/selenium-ide/src/neo/IO/SideeX/recorder.js
@@ -304,12 +304,17 @@ export default class BackgroundRecorder {
   }
 
   addCommandMessageHandler(message, sender, sendResponse) {
+    if (message.frameRemoved) {
+      browser.tabs.sendMessage(sender.tab.id, {
+        recalculateFrameLocation: true,
+      })
+      return sendResponse(true)
+    }
     if (message.requestFrameCount) {
       return sendResponse(this.windowSession.frameCountForTab[sender.tab.id])
     } else if (message.setFrameNumberForTab) {
       this.windowSession.frameCountForTab[sender.tab.id] = {
         indicatorIndex: message.indicatorIndex,
-        length: message.length,
       }
       return sendResponse(true)
     }
