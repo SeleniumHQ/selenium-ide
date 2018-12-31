49895017ed8ce799da524ae0331fbd9c3c72f9fd
David Haeffner <dhaeffner@gmail.com>
2018-12-31 10:55:09 +0200

    Content changes to the homepage

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -13,6 +13,14 @@ const MarkdownBlock = CompLibrary.MarkdownBlock /* Used to read markdown */
 const Container = CompLibrary.Container
 const GridBlock = CompLibrary.GridBlock
 
+const Button = props => (
+  <div className="pluginWrapper buttonWrapper">
+    <a className="button" href={props.href} target={props.target}>
+      {props.children}
+    </a>
+  </div>
+)
+
 class HomeSplash extends React.Component {
   render() {
     const { siteConfig } = this.props
@@ -38,14 +46,6 @@ class HomeSplash extends React.Component {
         <div className="promoRow">
           <div className="pluginRowBlock">{props.children}</div>
         </div>
-      </div>
-    )
-
-    const Button = props => (
-      <div className="pluginWrapper buttonWrapper">
-        <a className="button" href={props.href} target={props.target}>
-          {props.children}
-        </a>
       </div>
     )
 
@@ -110,41 +110,24 @@ class Index extends React.Component {
           align="center"
           contents={[
             {
-              content: `With the [Command-line runner for Selenium IDE](${_docUrl(
-                'command-line-runner'
-              )}) you can run your Selenium IDE tests in parallel, headlessly, on different brower & OS combinations, and connect it to a CI server.`,
+              title: 'Web Ready',
               image: `${baseUrl}img/parallel-execution.png`,
               imageAlign: 'top',
-              title: 'Fast Cross-browser Execution',
+              content: `Simple, turn-key solution to quickly author reliable end-to-end tests. Works out of the box for any web app.`,
             },
             {
-              content:
-                'Selenium IDE records multiple locators for each element it interacts with. If one locator fails during playback, the other locators will be tried until one is succesful.',
-              image: `${baseUrl}img/fallback.png`,
-              imageAlign: 'top',
-              title: 'Reslient Tests',
-            },
-            {
-              content: `Through the use of the [run command](${_docUrl(
-                'commands#run'
-              )}), you can re-use one test case insidie of another (e.g., allowing you to re-use your login logic in multiple places throughout a suite).`,
-              image: `${baseUrl}img/reuse.png`,
-              imageAlign: 'top',
-              title: 'Test Case Reuse',
-            },
-            {
-              content: `Extend Selenium IDE through the use of [plugins](${'plugins-getting-started'}). You can write plugins that will introduce new commands to the IDE or integrate it with a third-party service.`,
-              image: `${baseUrl}img/plugins.png`,
-              imageAlign: 'top',
-              title: 'Plugins',
-            },
-            {
-              content: `Download Selenium IDE for either [Chrome](${
-                downloadUrls.chrome
-              }) or [Firefox](${downloadUrls.firefox}) and get started.`,
+              title: 'Easy Debugging',
               image: `${baseUrl}img/browsers.png`,
               imageAlign: 'top',
-              title: 'Support for Chrome and Firefox',
+              content: `Enjoy easier test debugging with rich IDE features like setting breakpoints and pausing on exceptions.`,
+            },
+            {
+              title: 'Cross-browser Execution',
+              image: `${baseUrl}img/parallel-execution.png`,
+              imageAlign: 'top',
+              content: `Run your tests on any browser/OS combination in parallel using the [Command-line Runner for Selenium IDE](${_docUrl(
+                'introduction/command-line-runner'
+              )}).`,
             },
           ]}
         />
@@ -158,66 +141,85 @@ class Index extends React.Component {
       >
         <h2>Ready to use IDE</h2>
         <MarkdownBlock>
-          The IDE requires no additional setup other than installing the
-          extension on your browser. One of our driving philosophies is to
-          provide an easy to use tool, that will give instant feedback. We
-          believe that the easier we can make it, the better results the IDE
-          will have.
+          Getting started with Selenium IDE requires no additional setup other
+          than installing the extension on your browser. One of our driving
+          philosophies is to provide an easy to use tool that will give instant
+          feedback. We believe that the easier we can make it, the more likely
+          people are to author tests, which in turn results in better tested
+          apps.
         </MarkdownBlock>
       </div>
     )
 
-    const Debugger = () => (
+    const ResilientTests = () => (
       <Block background="light">
         {[
           {
+            title: 'Resilient Tests',
             content:
-              'Selenium IDE packs with it rich IDE features, including setting breakpoints, pausing on exceptions and more.',
-            image: `${baseUrl}img/debugger.png`,
+              'Selenium IDE records multiple locators for each element it interacts with. If one locator fails during playback, the others will be tried until one is succesful.',
+            image: `${baseUrl}img/fallback.png`,
             imageAlign: 'right',
-            title: 'Test Automation IDE',
+          },
+        ]}
+      </Block>
+    )
+
+    const TestCaseReuse = () => (
+      <Block>
+        {[
+          {
+            title: 'Test Case Reuse',
+            content: `Through the use of the [run command](${_docUrl(
+              'api/commands#run'
+            )}), you can re-use one test case insidie of another (e.g., allowing you to re-use your login logic in multiple places throughout a suite).`,
+            image: `${baseUrl}img/reuse.png`,
+            imageAlign: 'left',
           },
         ]}
       </Block>
     )
 
     const ControlFlow = () => (
-      <Block>
+      <Block background="light">
         {[
           {
-            content:
-              'Selenium IDE comes shipped with an extensive [control flow](control-flow-getting-started) structure, no more `gotos` and the likes, from now on `ifs`, `whiles` and `times` out of the box.',
+            title: 'Control Flow',
+            content: `Selenium IDE ships with an extensive control flow structure, with available commands like \`if\`, \`while\` and \`times\`. To learn more, check out [the Control Flow documentation](${_docUrl(
+              'introduction/control-flow'
+            )}).`,
             image: `${baseUrl}img/control-flow.png`,
             imageAlign: 'left',
-            title: 'Control Flow',
           },
         ]}
       </Block>
     )
 
-    const Runner = () => (
-      <Block background="light">
+    const Plugins = () => (
+      <Block>
         {[
           {
-            content:
-              "The [selenium-side-runner](runner-getting-started) is a feature packed test framework built on top of Selenium IDE. It let's you run tests in parallel, on different browsers, on Selenium Grid or on remove WebDriver instances.",
-            image: `${baseUrl}img/runner.png`,
-            imageAlign: 'right',
-            title: 'Selenium IDE Runner',
+            title: 'Plugins',
+            content: `Extend Selenium IDE through the use of [plugins](${_docUrl(
+              'plugins/plugins-getting-started'
+            )}). You can write plugins that will introduce new commands to the IDE or integrate it with a third-party service.`,
+            image: `${baseUrl}img/plugins.png`,
+            imageAlign: 'left',
           },
         ]}
       </Block>
     )
 
     const TryOut = () => (
-      <Block id="try">
+      <Block id="try" background="light">
         {[
           {
-            content:
-              '[Dave](https://twitter.com/tourdedave) explaining the ins-and-outs of the IDE',
-            image: `${baseUrl}img/selenium-ide128.png`,
+            title: 'Try it out',
+            image: `${baseUrl}img/browsers.png`,
             imageAlign: 'left',
-            title: 'Try it Out',
+            content: `Download Selenium IDE for either [Chrome](${
+              downloadUrls.chrome
+            }) or [Firefox](${downloadUrls.firefox}) and get started.`,
           },
         ]}
       </Block>
@@ -258,9 +260,10 @@ class Index extends React.Component {
         <div className="mainContainer">
           <Features />
           <FeatureCallout />
-          <Debugger />
+          <ResilientTests />
+          <TestCaseReuse />
           <ControlFlow />
-          <Runner />
+          <Plugins />
           <TryOut />
           <Showcase />
         </div>
