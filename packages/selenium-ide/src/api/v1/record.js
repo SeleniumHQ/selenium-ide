27ba75713b4457ac8af016da8d82f6d9e4038e53
Tomer <tomer@corevo.io>
2019-02-10 13:47:18 +0200

    allow plugins to edit commands at any time

--- packages/selenium-ide/src/api/v1/record.js
+++ packages/selenium-ide/src/api/v1/record.js
@@ -31,14 +31,49 @@ router.get('/tab', (_req, res) => {
   }
 })
 
+router.get('/command', (_req, res) => {
+  const test = UiState.displayedTest
+  if (test) {
+    res({ commands: test.commands.map(cmd => cmd.export()) })
+  } else {
+    res({ status: 'error', message: 'No test selected' })
+  }
+})
+
 router.post('/command', (req, res) => {
-  recordCommand(req.command, req.target, req.value, undefined, req.select)
+  recordCommand(req.command, req.target, req.value, req.index, req.select)
   const type =
     Commands.list.has(req.command) && Commands.list.get(req.command).type
   if (req.select && type) {
     select(type, undefined, true)
   }
   res(true)
+})
+
+router.put('/command', (req, res) => {
+  if (!req.id) {
+    res({ status: 'error', message: 'No id sent' })
+  } else {
+    const test = UiState.displayedTest
+    if (test) {
+      const command = test.commands.find(cmd => cmd.id === req.id)
+      if (command) {
+        command.setCommand(req.command || '')
+        command.setTarget(req.target || '')
+        command.setValue(req.value || '')
+        command.setTargets(Array.isArray(req.targets) ? req.targets : [])
+        res(true)
+      } else {
+        res({
+          status: 'error',
+          message:
+            'No command with that id exists in the currently displayed test',
+        })
+      }
+    } else {
+      res({ status: 'error', message: 'No test selected' })
+    }
+  }
 })
 
 export default router
