b16fbc0e11a2e3317b060c13b08c795464c63c10
Tomer <tomer@corevo.io>
2019-02-19 10:32:18 +0200

    make recording indicator a bit wider for windows

--- packages/selenium-ide/src/content/record-api.js
+++ packages/selenium-ide/src/content/record-api.js
@@ -187,7 +187,7 @@ function addRecordingIndicator() {
     recordingIndicator.style.position = 'fixed'
     recordingIndicator.style.bottom = '36px'
     recordingIndicator.style.right = '36px'
-    recordingIndicator.style.width = '380px'
+    recordingIndicator.style.width = '400px'
     recordingIndicator.style.height = '50px'
     recordingIndicator.style['background-color'] = '#f7f7f7'
     recordingIndicator.style['box-shadow'] = '0 7px 10px 0 rgba(0,0,0,0.1)'
