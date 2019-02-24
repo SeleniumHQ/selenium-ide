9b24d04fb2b7a1ec20d80ccf7801afc85cd138b9
Tomer <tomer@corevo.io>
2019-02-24 18:35:56 +0200

    fixed lint

--- packages/selenium-ide/src/neo/stores/view/SuiteState.js
+++ packages/selenium-ide/src/neo/stores/view/SuiteState.js
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-import { action, computed, observable } from 'mobx'
+import { computed } from 'mobx'
 
 export default class SuiteState {
   constructor(UiState, suite) {
