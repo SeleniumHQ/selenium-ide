f5272a6de1cc40594353f61ca8aa1587199d95f4
David Haeffner <dhaeffner@gmail.com>
2019-03-11 10:39:32 +0200

    Added beforeAll and afterAll to hooks/index.js

--- packages/code-export-java/src/hooks/index.js
+++ packages/code-export-java/src/hooks/index.js
@@ -1,11 +1,15 @@
 import dependencies from './declareDependencies'
 import variables from './declareVariables'
+import beforeAll from './beforeAll'
 import beforeEach from './beforeEach'
 import afterEach from './afterEach'
+import afterAll from './afterAll'
 
 export default {
   dependencies,
   variables,
+  beforeAll,
   beforeEach,
   afterEach,
+  afterAll,
 }
