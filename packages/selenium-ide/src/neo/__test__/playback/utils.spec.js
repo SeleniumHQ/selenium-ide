e9e453156549d506d911c2b24ad911b1cbc488f0
Tomer <tomer@corevo.io>
2019-03-20 16:24:01 +0200

    moved utils test

--- packages/selenium-ide/src/neo/__test__/playback/utils.spec.js
+++ packages/selenium-ide/src/neo/__test__/playback/utils.spec.js
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-import { absolutifyUrl } from '../../../IO/playback/utils'
+import { absolutifyUrl } from '../../playback/utils'
 
 describe('url absolutifier', () => {
   it('should append to the base url', () => {
