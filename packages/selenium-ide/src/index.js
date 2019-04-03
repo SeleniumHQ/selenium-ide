d481a8f64e08b37cebe4fe3f7acec37fa349faf1
Tomer <tomer@corevo.io>
2019-04-03 11:53:40 +0300

    initial selenium ide electron idea

---
+++ packages/selenium-ide/src/index.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron')
+const webdriver = require('selenium-webdriver')
+const chromedriver = require('chromedriver')
+
+app.commandLine.appendSwitch('remote-debugging-port', '8315')
+
+app.on('ready', async () => {
+  // Create the browser window.
+  let win = new BrowserWindow({
+    width: 1460,
+    height: 840,
+    webPreferences: { nodeIntegration: true, webviewTag: true },
+  })
+
+  // and load the index.html of the app.
+  win.loadFile(__dirname + '/index.html')
+
+  chromedriver.start()
+
+  const driver = new webdriver.Builder()
+    // The "9515" is the port opened by chrome driver.
+    .usingServer('http://localhost:9515')
+    .withCapabilities({
+      chromeOptions: {
+        // connect to the served electron DevTools
+        debuggerAddress: 'localhost:8315',
+        windowTypes: ['webview'],
+      },
+    })
+    .forBrowser('electron')
+    .build()
+
+  await driver.executeScript(
+    'document.getElementById("aut").src = "https://google.com"'
+  )
+})
