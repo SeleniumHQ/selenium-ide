601dfcc3bcbd08fa433a9cd86f80c87bce3a4489
Tomer <tomer@corevo.io>
2019-09-04 15:37:12 +0300

    command schema

---
+++ packages/side-model/__tests__/commands/command.spec.ts
@@ -0,0 +1,61 @@
+// Licensed to the Software Freedom Conservancy (SFC) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The SFC licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import Command from '../../src/commands/command'
+import ArgType from '../../src/args/arg-type'
+import Argument from '../../src/args/argument'
+
+describe('command', () => {
+  it('should have name and description', () => {
+    const cmd = new Command({
+      name: 'cmd',
+      description: 'desc',
+      validate: () => true,
+    })
+
+    expect(cmd.name).toBe('cmd')
+    expect(cmd.description).toBe('desc')
+  })
+
+  it('should have a validation function', () => {
+    const cmd = new Command({
+      name: 'cmd',
+      description: 'desc',
+      validate: (n: number) => n === 1,
+    })
+
+    expect(cmd.validate(1)).toBeTruthy()
+    expect(cmd.validate(2)).toBeFalsy()
+  })
+
+  it('should have an arg-type', () => {
+    const arg = new Argument({
+      name: 'arg',
+      description: 'arg',
+      identify: (n: number) => typeof n === 'number',
+      validate: (n: number) => n === 1,
+    })
+    const at = new ArgType([arg])
+    const cmd = new Command({
+      name: 'cmd',
+      description: 'desc',
+      validate: (n: number) => at.validate(n),
+    })
+    expect(cmd.validate(1)).toBeTruthy()
+    expect(cmd.validate(2)).toBeFalsy()
+  })
+})
