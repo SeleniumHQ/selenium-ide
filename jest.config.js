847b17b80b18726c6ba10b7cf5b2bdf35984824a
Rajendra Kadam <rajendrakadam249@gmail.com>
2020-10-30 09:25:40 +0530

    Add test target for side-model

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
   testEnvironment: 'node',
   reporters: ['default'],
   transform: { '^.+\\.jsx?$': 'babel-jest' },
-  testMatch: ['**/__tests__/*.js'],
+  testMatch: ['**/__tests__/*.js', '**/__tests__/**/*.js'],
   moduleNameMapper: {
     'seleniumide/(.*)': '<rootDir>/$1',
   }
