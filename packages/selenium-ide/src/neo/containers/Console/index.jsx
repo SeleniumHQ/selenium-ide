42d7940afeca816a0b021fcfbbf0a569becc1174
melode11 <melode11@gmail.com>
2021-02-01 06:10:25 -0800

    Add arrow key controlling for switching the Console tab bar. (#1186)
    * Add arrow key controlling for switching the tab bar.
    
    * Fix jslint issues.
    
    * Fix remaining lint issue.
    
    * Fix tablist attrib position and fix typo.
    
    Co-authored-by: Yuxing Yao <yuxyao@microsoft.com>

--- packages/selenium-ide/src/neo/containers/Console/index.jsx
+++ packages/selenium-ide/src/neo/containers/Console/index.jsx
@@ -89,10 +89,19 @@ export default class Console extends React.Component {
         </TabBar>
         <div className="viewport" ref={this.setViewportRef}>
           {this.state.tab === 'Log' && (
-            <LogList output={output} scrollTo={this.scroll} />
+            <LogList
+              output={output}
+              scrollTo={this.scroll}
+              id="Log"
+              role="tabpanel"
+            />
           )}
           {this.state.tab === 'Reference' && (
-            <CommandReference currentCommand={command} />
+            <CommandReference
+              currentCommand={command}
+              id="Reference"
+              role="tabpanel"
+            />
           )}
         </div>
       </footer>
