4e0dce78d3357971a259205716983bb03f7f1fef
Brandon <brandon.piner@hpe.com>
2021-07-19 10:48:44 +0200

    Fixed name

--- packages/code-export/__test__/src/index.spec.js
+++ packages/code-export/__test__/src/index.spec.js
@@ -2,12 +2,12 @@ import { emitCommand } from '../../src'
 
 describe('Code Export Python pytest', () => {
   it('should export a test', async () => {
-    let x = await emitCommand('python-pytest', {
+    let command = await emitCommand('python-pytest', {
       command: 'click',
       target: "//form[input/@name='email']",
       value: '',
     })
-    expect(x).toEqual(
+    expect(command).toEqual(
       'self.driver.find_element(By.XPATH, "//form[input/@name=\\\'email\\\']").click()'
     )
   })
