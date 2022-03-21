8e4195b4aeba33c678c74591530f6cedea7bfce6
toddtarsi <tarsitodd@gmail.com>
2022-03-20 22:09:40 -0500

    drag and drop feature for test steps

--- packages/selenium-ide/src/browser/windows/ProjectEditor/renderer.tsx
+++ packages/selenium-ide/src/browser/windows/ProjectEditor/renderer.tsx
@@ -9,6 +9,8 @@ import loadingID from 'api/constants/loadingID'
 import AppWrapper from 'browser/components/AppWrapper'
 import subscribeToSession from 'browser/helpers/subscribeToSession'
 import React from 'react'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 import SuitesDrawer from './components/Suites/SuitesDrawer'
 import SuitesTab from './components/Suites/SuitesTab'
 import TestsDrawer from './components/Tests/TestsDrawer'
@@ -44,66 +46,68 @@ const ProjectTestCommandList = () => {
 
   return (
     <AppWrapper>
-      <Box sx={{ display: 'flex' }}>
-        <AppBar className="draggable" position="fixed" open={openDrawer}>
-          <Toolbar className="" variant="dense">
-            <TabPanelMulti indexes={[0, 1]} value={tab}>
-              <IconButton
-                color="inherit"
-                aria-label="openDrawer drawer"
-                onClick={() => setOpenDrawer(true)}
-                edge="start"
-                sx={{ mr: 2, ...(openDrawer && { display: 'none' }) }}
-              >
-                <MenuIcon />
-              </IconButton>
-            </TabPanelMulti>
-            <TabPanel index={2} value={tab} />
-            <Typography variant="h6" noWrap component="div">
-              Selenium IDE
-            </Typography>
-          </Toolbar>
-          <Tabs
-            value={tab}
-            onChange={(_e, v) => setTab(v)}
-            indicatorColor="secondary"
-            textColor="inherit"
-            variant="fullWidth"
-            aria-label="Selenium IDE workflows"
-          >
-            <Tab label="Tests" {...a11yProps(0)} />
-            <Tab label="Suites" {...a11yProps(1)} />
-            <Tab label="Config" {...a11yProps(2)} />
-          </Tabs>
-        </AppBar>
-        <TabPanel index={0} value={tab}>
-          <TestsDrawer
-            activeTest={activeTestID}
-            open={openDrawer}
-            setOpen={setOpenDrawer}
-            tests={tests}
-          />
-        </TabPanel>
-        <TabPanel index={1} value={tab}>
-          <SuitesDrawer
-            activeSuite={activeSuiteID}
-            open={openDrawer}
-            setOpen={setOpenDrawer}
-            suites={suites}
-          />
-        </TabPanel>
-        <Main className="fill no-select" open={openDrawer}>
+      <DndProvider backend={HTML5Backend}>
+        <Box sx={{ display: 'flex' }}>
+          <AppBar className="draggable" position="fixed" open={openDrawer}>
+            <Toolbar className="" variant="dense">
+              <TabPanelMulti indexes={[0, 1]} value={tab}>
+                <IconButton
+                  color="inherit"
+                  aria-label="openDrawer drawer"
+                  onClick={() => setOpenDrawer(true)}
+                  edge="start"
+                  sx={{ mr: 2, ...(openDrawer && { display: 'none' }) }}
+                >
+                  <MenuIcon />
+                </IconButton>
+              </TabPanelMulti>
+              <TabPanel index={2} value={tab} />
+              <Typography variant="h6" noWrap component="div">
+                Selenium IDE
+              </Typography>
+            </Toolbar>
+            <Tabs
+              value={tab}
+              onChange={(_e, v) => setTab(v)}
+              indicatorColor="secondary"
+              textColor="inherit"
+              variant="fullWidth"
+              aria-label="Selenium IDE workflows"
+            >
+              <Tab label="Tests" {...a11yProps(0)} />
+              <Tab label="Suites" {...a11yProps(1)} />
+              <Tab label="Config" {...a11yProps(2)} />
+            </Tabs>
+          </AppBar>
           <TabPanel index={0} value={tab}>
-            <TestsTab session={session} />
+            <TestsDrawer
+              activeTest={activeTestID}
+              open={openDrawer}
+              setOpen={setOpenDrawer}
+              tests={tests}
+            />
           </TabPanel>
           <TabPanel index={1} value={tab}>
-            <SuitesTab session={session} />
+            <SuitesDrawer
+              activeSuite={activeSuiteID}
+              open={openDrawer}
+              setOpen={setOpenDrawer}
+              suites={suites}
+            />
           </TabPanel>
-          <TabPanel index={2} value={tab}>
-            <ProjectTab open={openDrawer} session={session} />
-          </TabPanel>
-        </Main>
-      </Box>
+          <Main className="fill no-select" open={openDrawer}>
+            <TabPanel index={0} value={tab}>
+              <TestsTab session={session} />
+            </TabPanel>
+            <TabPanel index={1} value={tab}>
+              <SuitesTab session={session} />
+            </TabPanel>
+            <TabPanel index={2} value={tab}>
+              <ProjectTab open={openDrawer} session={session} />
+            </TabPanel>
+          </Main>
+        </Box>
+      </DndProvider>
     </AppWrapper>
   )
 }
