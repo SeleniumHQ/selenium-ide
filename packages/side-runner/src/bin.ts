80280a42ef03ec1cb52d930193b508c392cd1eeb
toddtarsi <tarsitodd@gmail.com>
2022-04-06 00:15:45 -0500

    run side-runner within jest

--- packages/side-runner/src/bin.ts
+++ packages/side-runner/src/bin.ts
@@ -17,18 +17,15 @@
 // specific language governing permissions and limitations
 // under the License.
 
-import fs from 'fs'
 import path from 'path'
 import crypto from 'crypto'
 import util from 'util'
 import { Command } from 'commander'
-import glob from 'glob'
-import { createLogger, format, transports } from 'winston'
 import Capabilities from './capabilities'
 import Config from './config'
 import ParseProxy from './proxy'
-import { Configuration, JSON, Project, SideRunnerAPI } from './types'
-import buildRunners from './run'
+import { Configuration, JSON, SideRunnerAPI } from './types'
+import { spawn } from 'child_process'
 
 const metadata = require('../package.json')
 
@@ -40,6 +37,7 @@ const program: SideRunnerAPI = new Command() as SideRunnerAPI
 program
   .usage('[options] project.side [project.side] [*.side]')
   .version(metadata.version)
+  .option('--base-url [url]', 'Override the base URL that was set in the IDE')
   .option('-c, --capabilities [list]', 'Webdriver capabilities')
   .option('-s, --server [url]', 'Webdriver remote server')
   .option('-p, --params [list]', 'General parameters')
@@ -48,7 +46,6 @@ program
     '-w, --max-workers [number]',
     'Maximum amount of workers that will run your tests, defaults to number of cores'
   )
-  .option('--base-url [url]', 'Override the base URL that was set in the IDE')
   .option(
     '--timeout [number | undefined]',
     `The maximimum amount of time, in milliseconds, to spend attempting to locate an element. (default: ${DEFAULT_TIMEOUT})`
@@ -62,7 +59,7 @@ program
     'Proxy options to pass, for use with manual, pac and socks proxies'
   )
   .option(
-    '--config, --config-file [filepath]',
+    '--config-file [filepath]',
     'Use specified YAML file for configuration. (default: .side.yml)'
   )
   .option(
@@ -70,25 +67,10 @@ program
     'Write test results to files, format is defined by --output-format'
   )
   .option(
-    '--output-format [jest | junit]',
-    'Format for the output. (default: jest)'
-  )
-  .option(
     '--force',
     "Forcibly run the project, regardless of project's version"
   )
   .option('--debug', 'Print debug logs')
-
-if (process.env.NODE_ENV === 'development') {
-  program.option(
-    '-e, --extract',
-    'Only extract the project file to code (this feature is for debugging purposes)'
-  )
-  program.option(
-    '-r, --run [directory]',
-    'Run the extracted project files (this feature is for debugging purposes)'
-  )
-}
 
 program.parse(process.argv)
 
@@ -98,32 +80,22 @@ if (!program.args.length) {
   process.exit(1)
 }
 
-const logger = createLogger({
-  level: program.debug ? 'debug' : 'info',
-  transports: [
-    new transports.Console({
-      format: format.combine(format.colorize(), format.simple()),
-    }),
-  ],
-})
-
-if (program.extract || program.run) {
-  logger.warn(
-    "This feature is used by Selenium IDE maintainers for debugging purposes, we hope you know what you're doing!"
-  )
-}
-
 const configuration: Configuration = {
   baseUrl: '',
   capabilities: {
     browserName: 'chrome',
   },
+  debug: program.debug,
+  filter: '*',
+  force: program.force,
+  maxWorkers: program.maxWorkers,
   params: {},
+  projects: program.args,
   proxyOptions: {},
   runId: crypto.randomBytes(16).toString('hex'),
   path: path.join(__dirname, '../../'),
   server: '',
-  timeout: 0,
+  timeout: DEFAULT_TIMEOUT,
 }
 
 const confPath = program.configFile || '.side.yml'
@@ -133,28 +105,23 @@ const configFilePath = path.isAbsolute(confPath)
 try {
   Object.assign(configuration, Config.load(configFilePath))
 } catch (e) {
-  logger.debug('Could not load ' + configFilePath)
+  program.debug && console.debug('Could not load ' + configFilePath)
 }
 
-program.filter = program.filter || '*'
+if (program.filter) {
+  configuration.filter = program.filter
+}
 configuration.server = program.server ? program.server : configuration.server
 
-configuration.timeout = program.timeout
-  ? +program.timeout
-  : configuration.timeout
-  ? +configuration.timeout
-  : DEFAULT_TIMEOUT // eslint-disable-line indent
-
-// @ts-expect-error
-if (configuration.timeout === 'undefined') {
-  configuration.timeout = undefined
+if (program.timeout) {
+  configuration.timeout = +program.timeout
 }
 
 if (program.capabilities) {
   try {
     configuration.capabilities = Capabilities.parseString(program.capabilities)
   } catch (e) {
-    logger.debug('Failed to parse inline capabilities')
+    program.debug && console.debug('Failed to parse inline capabilities')
   }
 }
 
@@ -162,7 +129,7 @@ if (program.params) {
   try {
     configuration.params = Capabilities.parseString(program.params)
   } catch (e) {
-    logger.debug('Failed to parse additional params')
+    program.debug && console.debug('Failed to parse additional params')
   }
 }
 
@@ -175,7 +142,7 @@ if (program.proxyType) {
     const proxy = ParseProxy(program.proxyType, opts as Record<string, JSON>)
     Object.assign(configuration, proxy)
   } catch (e) {
-    logger.error((e as Error).message)
+    console.error((e as Error).message)
     // eslint-disable-next-line no-process-exit
     process.exit(1)
   }
@@ -187,7 +154,7 @@ if (program.proxyType) {
     )
     Object.assign(configuration, proxy)
   } catch (e) {
-    logger.error((e as Error).message)
+    console.error((e as Error).message)
     // eslint-disable-next-line no-process-exit
     process.exit(1)
   }
@@ -197,32 +164,12 @@ configuration.baseUrl = program.baseUrl
   ? program.baseUrl
   : configuration.baseUrl
 
-logger.debug(util.inspect(configuration))
+program.debug && console.debug(util.inspect(configuration))
 
-function handleQuit(_signal: string, code: number) {
-  // eslint-disable-next-line no-process-exit
-  process.exit(code)
-}
-process.on('SIGINT', handleQuit)
-process.on('SIGTERM', handleQuit)
-
-const projects: Project[] = [
-  ...program.args.reduce((projects, project) => {
-    glob.sync(project).forEach((p) => {
-      projects.add(p)
-    })
-    return projects
-  }, new Set<string>()),
-].map((p) => {
-  const project = JSON.parse(fs.readFileSync(p, 'utf8'))
-  project.path = p
-  return project
+spawn('jest', ['--verbose', path.join(__dirname, 'main.test.js')], {
+  env: {
+    ...process.env,
+    SE_CONFIGURATION: JSON.stringify(configuration),
+  },
+  stdio: 'inherit',
 })
-
-const runners = buildRunners({
-  configuration,
-  logger: logger as unknown as Console,
-  program,
-})
-
-runners.all(projects)
---
+++ packages/side-runner/src/main.test.ts
@@ -0,0 +1,80 @@
+// Licensed to the Software Freedom Conservancy (SFC) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The SFC licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import each from 'jest-each'
+import fs from 'fs'
+import util from 'util'
+import glob from 'glob'
+import { createLogger, format, transports } from 'winston'
+import { Configuration, Project } from './types'
+import buildRegister from './register'
+import buildRunners from './run'
+import { SuiteShape } from '@seleniumhq/side-model'
+
+const metadata = require('../package.json')
+
+process.title = metadata.name
+
+const configuration: Configuration = JSON.parse(
+  process.env.SE_CONFIGURATION as string
+)
+jest.setTimeout(parseInt('' + configuration.timeout))
+
+const logger = createLogger({
+  level: configuration.debug ? 'debug' : 'info',
+  transports: [
+    new transports.Console({
+      format: format.combine(format.colorize(), format.simple()),
+    }),
+  ],
+})
+
+logger.debug(util.inspect(configuration))
+
+describe('Running all of the selected projects', () => {
+  const projects: Project[] = [
+    ...configuration.projects.reduce((projects, project) => {
+      glob.sync(project).forEach((p) => {
+        projects.add(p)
+      })
+      return projects
+    }, new Set<string>()),
+  ].map((p) => {
+    const project = JSON.parse(fs.readFileSync(p, 'utf8'))
+    project.path = p
+    return project
+  })
+  const factoryParams = {
+    configuration,
+    logger: logger as unknown as Console,
+  }
+  const register = buildRegister(factoryParams)
+  const runners = buildRunners(factoryParams)
+  each(projects).describe('Running project $name', (project: Project) => {
+    each(project.suites).describe(
+      'Running suite $name',
+      (suite: SuiteShape) => {
+        each(suite.tests.map((tID) => register.test(project, tID))).it(
+          'Running tests $name',
+          async (test) => {
+            await runners.test(project, test)
+          }
+        )
+      }
+    )
+  })
+})
