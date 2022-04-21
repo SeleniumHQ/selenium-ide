ad0d05fef2d87217014592d8992d59644098f779
Robert Castles <rcastles@pmg.net>
2022-04-21 00:18:39 -0400

    improve logic to delete windows that do not start with projectEditorName

--- packages/selenium-ide/src/main/session/controllers/Windows/index.ts
+++ packages/selenium-ide/src/main/session/controllers/Windows/index.ts
@@ -123,7 +123,8 @@ export default class WindowsController {
     }
     this.windows[name] = window
     window.on('closed', () => {
-      if (name != 'project-editor') delete this.windows[name]
+      if (!name.startsWith(projectEditorWindowName))
+        delete this.windows[name]
     })
     return true
   }
