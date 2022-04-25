3d0e043350a0c135ff23c54449468ad3583e8d48
Robert Castles <rcastles@pmg.net>
2022-04-25 16:27:29 -0400

    sort suites by name

--- packages/selenium-ide/src/browser/windows/ProjectEditor/components/Suites/SuitesDrawer.tsx
+++ packages/selenium-ide/src/browser/windows/ProjectEditor/components/Suites/SuitesDrawer.tsx
@@ -49,16 +49,27 @@ const SuiteList: FC<SuiteListProps> = ({
         />
       }
     >
-      {suites.map(({ id, name }) => (
-        <RenamableListItem
-          id={id}
-          key={id}
-          name={name}
-          rename={rename}
-          selected={id === activeSuite}
-          setSelected={setSelected}
-        />
-      ))}
+      {suites
+        .slice()
+        .sort((a, b) => {
+          if (b.name < a.name) {
+            return 1
+          }
+          if (a.name < b.name) {
+            return -1
+          }
+          return 0
+        })
+        .map(({ id, name }) => (
+          <RenamableListItem
+            id={id}
+            key={id}
+            name={name}
+            rename={rename}
+            selected={id === activeSuite}
+            setSelected={setSelected}
+          />
+        ))}
     </List>
   </Drawer>
 )
