33027ca8a11ad5f61268db9d4a0b6d80f8125938
coinzdude <58857646+coinzdude@users.noreply.github.com>
2022-05-01 20:45:49 -0400

    Check for save on load/exit; partially working (#1401)
    initial pass to get save confirmation on project load, app close - close not working, cancel not working
    
    Authored-by: Robert Castles <rcastles@pmg.net>

--- packages/selenium-ide/src/main/session/controllers/Dialogs/index.ts
+++ packages/selenium-ide/src/main/session/controllers/Dialogs/index.ts
@@ -4,6 +4,14 @@ export default class DialogsController {
   async open() {
     return await dialog.showOpenDialog({ properties: ['openFile'] })
   }
+  async showMessageBox(
+    message: string,
+    buttons: string[]
+  ): Promise<number>
+  {
+    const { response } = await dialog.showMessageBox({ message, buttons })
+    return response
+  }
   async confirm(
     options: Electron.MessageBoxOptions = {
       message: 'Are you sure?',
@@ -17,4 +25,14 @@ export default class DialogsController {
   async openSave() {
     return await dialog.showSaveDialog({})
   }
+  // async promptSave(
+  //   options: Electron.MessageBoxOptions = {
+  //     message: 'Save changes to current project?',
+  //     buttons: ['Yes', 'No'],
+  //   }
+  // ): Promise<boolean> {
+  //   const { response } = await dialog.showMessageBox(options)
+  //   const confirmed = response === 0
+  //   return confirmed
+  // }
 }
