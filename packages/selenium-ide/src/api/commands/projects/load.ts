a5658c375473c656a25266639a3070e7f0ca5502
Robert Castles <rcastles@pmg.net>
2022-05-04 16:34:07 -0400

    updated to not use default, cannot load project

--- packages/selenium-ide/src/api/commands/projects/load.ts
+++ packages/selenium-ide/src/api/commands/projects/load.ts
@@ -1,4 +1,4 @@
-import defaultState from 'api/models/state'
+// import defaultState from 'api/models/state'
 import browserHandler from 'browser/api/classes/Handler'
 import mainHandler from 'main/api/classes/Handler'
 import { Session } from 'main/types'
@@ -18,7 +18,7 @@ export const mutator: Mutator<Shape> = (session, { result }) => {
   if (!result) {
     return session
   }
-  result.state = defaultState
+  // result.state = defaultState
   const { project, state } = result
   if (state) {
     return result
