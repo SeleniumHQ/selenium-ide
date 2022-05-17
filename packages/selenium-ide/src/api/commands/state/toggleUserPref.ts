ec121c566832c47f26357b7e45b50392a14cd394
Robert Castles <rcastles@pmg.net>
2022-05-17 15:14:44 -0400

    removed passthrough

--- packages/selenium-ide/src/api/commands/state/toggleUserPref.ts
+++ packages/selenium-ide/src/api/commands/state/toggleUserPref.ts
@@ -1,21 +1,17 @@
+import browserHandler from 'browser/api/classes/Handler'
 import { UserPrefs } from 'api/types'
 import update from 'lodash/fp/update'
+import mainHandler, { passthrough } from 'main/api/classes/Handler'
 import storage from 'main/store'
 import { Mutator } from 'api/types'
 
 export type Shape = (prefs: UserPrefs) => Promise<void>
 
-export const mutator: Mutator<Shape> = (session, { params: [userPrefs] }) => 
-  update(
-    'state.userprefs',
-    () =>
-      setUserPrefs(userPrefs),
-    session
-  )
-
+export const mutator: Mutator<Shape> = (session, { params: [userPrefs] }) =>
+  update('state.userprefs', () => setUserPrefs(userPrefs), session)
 
 export const setUserPrefs = (prefs: UserPrefs) => {
-    storage.set<'userPrefs'>('userPrefs', prefs)
+  storage.set<'userPrefs'>('userPrefs', prefs)
 }
 
 export const getUserPrefs = (): UserPrefs => {
@@ -27,3 +23,6 @@ export const getUserPrefs = (): UserPrefs => {
   return userPrefs
 }
 
+export const browser = browserHandler<Shape>()
+
+export const main = mainHandler<Shape>()
