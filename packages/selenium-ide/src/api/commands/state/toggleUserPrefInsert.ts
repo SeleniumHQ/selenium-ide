5cd8003600f60d69324e07c9d5a752981ac5e39b
Robert Castles <rcastles@pmg.net>
2022-05-23 23:50:06 -0400

    some updates attempting to work through the user preference setting

--- packages/selenium-ide/src/api/commands/state/toggleUserPrefInsert.ts
+++ packages/selenium-ide/src/api/commands/state/toggleUserPrefInsert.ts
@@ -1,23 +1,20 @@
+import update from 'lodash/fp/update'
 import browserHandler from 'browser/api/classes/Handler'
 import mainHandler from 'main/api/classes/Handler'
-import { Mutator } from 'api/types'
+import { Mutator, UserPrefs } from 'api/types'
 import { Session } from 'main/types'
 
 export type Shape = Session['state']['toggleUserPrefInsert']
 
-export const mutator: Mutator<Shape> = (
-  session,
-  { params: [insertCommandPref] }
-) => ({
-  ...session,
-  state: {
-    ...session.state,
-    userPrefs: {
-      ...session.state.userPrefs,
-      insertCommandPref: insertCommandPref
-    },
-  },
-})
+export const mutator: Mutator<Shape> = (session, { params: [insertCommandPref] }) =>
+  update(
+    'state.userprefs.insertCommandPref',
+    (userPrefs: UserPrefs) =>
+      userPrefs.insertCommandPref = insertCommandPref
+      ,
+    session
+  )
+
 export const browser = browserHandler<Shape>()
 
 export const main = mainHandler<Shape>()
