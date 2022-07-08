e4466a9a2c857428382ac6b5cf547ec2922e9c76
Robert Castles <58857646+coinzdude@users.noreply.github.com>
2022-07-08 12:55:34 -0400

    New dialogs enter key and z index (#1490)
    * Dialog prompt for new suite

--- packages/selenium-ide/src/browser/windows/ProjectEditor/tabs/Suites/SuiteNewDialog.tsx
+++ packages/selenium-ide/src/browser/windows/ProjectEditor/tabs/Suites/SuiteNewDialog.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
 import Button from '@mui/material/Button'
-import TextField from '@mui/material/TextField'
 import Dialog from '@mui/material/Dialog'
 import DialogActions from '@mui/material/DialogActions'
 import DialogContent from '@mui/material/DialogContent'
 import DialogContentText from '@mui/material/DialogContentText'
+import TextField from '@mui/material/TextField'
 
 const {
   state: { setActiveSuite: setSelected },
@@ -22,35 +22,46 @@ const SuiteNewDialog: React.FC<SuiteNewDialogProps> = ({ confirmNew, setConfirmN
 
   const handleClose = async (value: CloseReason) => {
     if (value === 'Create') {
-      const activeSuite = (await window.sideAPI.suites.create(suiteName)).id
-      setSelected(activeSuite)
+      createSuite()
     }
     setConfirmNew(false)
   }
 
+  const onKeyDown = (event: React.KeyboardEvent<HTMLDivElement>): void => {
+    // 'keypress' event misbehaves on mobile so we track 'Enter' key via 'keydown' event
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      event.stopPropagation()
+      createSuite()
+      setConfirmNew(false)
+    }
+  }
+
+  const createSuite = async () => {
+    const activeSuite = (await window.sideAPI.suites.create(suiteName)).id
+    setSelected(activeSuite)
+  }
+
   return (
-    <div>
-      <Dialog open={confirmNew} onClose={handleClose}>
-        <DialogContent>
-          <DialogContentText>
-            Please specify the new suite name
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Suite Name"
-            fullWidth
-            variant="standard"
-            onChange={(e) => setSuiteName(e.target.value)}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={() => handleClose('Cancel')}>Cancel</Button>
-          <Button onClick={() => handleClose('Create')}>Create</Button>
-        </DialogActions>
-      </Dialog>
-    </div>
+    <Dialog open={confirmNew} onClose={handleClose} style={{ zIndex: 99999 }}>
+      <DialogContent>
+        <DialogContentText>Please specify the new suite name</DialogContentText>
+        <TextField
+          autoFocus
+          fullWidth
+          id="name"
+          label="Suite Name"
+          margin="dense"
+          onChange={(e) => setSuiteName(e.target.value)}
+          onKeyDown={onKeyDown}
+          variant="standard"
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => handleClose('Cancel')}>Cancel</Button>
+        <Button onClick={() => handleClose('Create')}>Create</Button>
+      </DialogActions>
+    </Dialog>
   )
 }
 
